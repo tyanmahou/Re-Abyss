8fd85d4f2a4e5369ae2ca308e78dfca695a17d3e
mahou <tyanmahou@gmail.com>
2019-09-01 00:56:32 +0900

    add visiter

--- Re-Abyss/apps/Main.cpp
+++ Re-Abyss/apps/Main.cpp
@@ -1,24 +1,7 @@
 ﻿#include<application/common/AbyssDream.hpp>
 
-#include<application/util/Visitor.hpp>
-
-struct MyStruct
-{
-	void operator()(int)const {
-
-	}
-};
-struct MyStruct2
-{
-	void operator()(double)const {
-
-	}
-};
 void Main()
 {
-	auto f = abyss::overloaded(
-		[](int) {}
-	);
 	// アプリ起動
 	abyss::AbyssDream app;
 	app.run();
--- Re-Abyss/apps/application/util/Visitor.hpp
+++ Re-Abyss/apps/application/util/Visitor.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include<utility>
+#include<memory>
 
 namespace abyss
 {
+
 	template<class Head, class... Tail>
 	struct overloaded_impl : Head, overloaded_impl<Tail...>
 	{
@@ -22,9 +24,119 @@ namespace abyss
 		using Last::operator();
 	};
 
+	/// <summary>
+	///　関数オブジェクトのオーバーロード
+	/// </summary>
 	template<class... Type>
 	overloaded_impl<Type...> overloaded(Type&& ... func)
 	{
 		return overloaded_impl<Type... >{std::forward<Type>(func)...};
+	};
+
+
+	/// <summary>
+	/// ビジター
+	/// </summary>
+	template<class... Accepter>
+	class Visitor
+	{
+		template<class U>
+		struct _ivisiter
+		{
+			virtual void visit(U&) const = 0;
+			virtual ~_ivisiter() = default;
+		};
+
+		template<class Head, class... Tail>
+		struct _base2 : _ivisiter<Head>, _base2<Tail...>
+		{
+			using _ivisiter<Head>::visit;
+			using _base2<Tail...>::visit;
+		};
+		template<class Last>
+		struct _base2<Last> : _ivisiter<Last>
+		{
+			using _ivisiter<Last>::visit;
+		};
+		struct _base : _base2<Accepter...>
+		{
+			using _base2<Accepter...>::visit;
+		};
+
+		template<class F, class Void, class Head, class... Tail>
+		struct _func2 : _func2<F, Void, Tail...>
+		{
+			void visit(Head&) const override
+			{};
+			using _func2<F, Void, Tail...>::_func2;
+			using _func2<F, Void, Tail...>::visit;
+		};
+
+		template<class F, class Head, class... Tail>
+		struct _func2<
+			F,
+			decltype(std::declval<F>()(std::declval<Head&>())),
+			Head, 
+			Tail...
+        > : _func2<F, void,Tail...>
+		{
+			void visit(Head& a) const override
+			{
+				return this->m_func(a);
+			};
+			using _func2<F, void, Tail...>::_func2;
+			using _func2<F, void, Tail...>::visit;
+		};
+
+		template<class F, class Void, class Last>
+		struct _func2<F, Void, Last> : _base
+		{
+			F m_func;
+			_func2(F f) :
+				m_func(f)
+			{}
+			void visit(Last&) const override
+			{};
+			using _base::visit;
+		};
+
+		template<class F, class Last>
+		struct _func2<
+			F, 
+			decltype(std::declval<F>()(std::declval<Last&>())),
+			Last
+		> : _base
+		{
+			F m_func;
+			_func2(F f) :
+				m_func(f)
+			{}
+			void visit(Last& a) const override
+			{
+				return m_func(a);
+			};
+			using _base::visit;
+		};
+
+		template<class F>
+		struct _func : _func2<F, void, Accepter...>
+		{
+			using _func2<F, void, Accepter...>::_func2;
+			using _func2<F, void, Accepter...>::visit;
+		};
+
+		std::unique_ptr<_base> m_storage;
+	public:
+
+		template<class F>
+		Visitor(F&& f) :
+			m_storage(std::make_unique<_func<F>>(std::forward<F>(f)))
+		{}
+
+		template<class Arg>
+		void visit(Arg&& arg)
+		{
+			return m_storage->visit(std::forward<Arg>(arg));
+		}
 	};
 }
