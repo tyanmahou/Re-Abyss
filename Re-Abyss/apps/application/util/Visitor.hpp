559b1e5b58c9e3f89b11d76de065d33d5887c666
mahou <tyanmahou@gmail.com>
2019-09-01 17:16:23 +0900

    fix

--- Re-Abyss/apps/application/util/Visitor.hpp
+++ Re-Abyss/apps/application/util/Visitor.hpp
@@ -4,34 +4,16 @@
 
 namespace abyss
 {
-
-	template<class Head, class... Tail>
-	struct overloaded_impl : Head, overloaded_impl<Tail...>
-	{
-		overloaded_impl(Head h, Tail... tail) :
-			Head(std::forward<Head>(h)),
-			overloaded_impl<Tail...>(std::forward<Tail>(tail)...)
-		{}
-		using Head::operator();
-		using overloaded_impl<Tail...>::operator();
-	};
-	template<class Last>
-	struct overloaded_impl<Last> : Last
-	{
-		overloaded_impl(Last last) :
-			Last(std::forward<Last>(last))
-		{}
-		using Last::operator();
-	};
-
 	/// <summary>
 	///　関数オブジェクトのオーバーロード
 	/// </summary>
 	template<class... Type>
-	overloaded_impl<Type...> overloaded(Type&& ... func)
+	struct overloaded : Type...
 	{
-		return overloaded_impl<Type... >{std::forward<Type>(func)...};
+		using Type::operator()...;
 	};
+	template<class... Type>
+	overloaded(Type ...)->overloaded<Type...>;
 
 
 	/// <summary>
