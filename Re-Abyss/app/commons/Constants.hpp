bb187b1af4b03617a736e519e85eb16d2ab08eee
mahou <tyanmahou@gmail.com>
2020-02-10 23:41:53 +0900

    lib link and add Constants.hpp

---
+++ Re-Abyss/app/commons/Constants.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <Siv3D/Vector2D.hpp>
+
+/// <summary>
+/// 定数
+/// </summary>
+namespace abyss::Constants
+{
+    // アプリ名
+    inline constexpr auto* const AppName = U"Abyss Dream";
+    // 解像度
+    inline constexpr s3d::Size AppResolution{ 960, 540 };
+}
