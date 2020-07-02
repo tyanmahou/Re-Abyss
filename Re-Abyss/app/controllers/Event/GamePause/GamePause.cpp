618f3b3617a488a816717136a3a01ad5aae0ed22
tyanmahou <tyanmahou@gmail.com>
2020-07-03 02:24:00 +0900

    add pause event mock

---
+++ Re-Abyss/app/controllers/Event/GamePause/GamePause.cpp
@@ -0,0 +1,21 @@
+#include "GamePause.hpp"
+#include <abyss/controllers/Manager/Manager.hpp>
+#include <abyss/controllers/GlobalTime/GlobalTime.hpp>
+
+namespace abyss::Event
+{
+    GamePause::GamePause()
+    {}
+    void GamePause::init()
+    {
+        m_pManager->getModule<GlobalTime>()->pause();
+    }
+    bool GamePause::update([[maybe_unused]]double dt)
+    {
+        return true;
+    }
+    void GamePause::onEnd()
+    {
+        m_pManager->getModule<GlobalTime>()->resume();
+    }
+}
---
+++ Re-Abyss/app/controllers/Event/GamePause/GamePause.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <abyss/commons/Fwd.hpp>
+#include <abyss/controllers/Event/base/IEvent.hpp>
+#include <abyss/utils/Ref/Ref.hpp>
+
+
+namespace abyss::Event
+{
+    class GamePause : public IEvent
+    {
+    private:
+
+    public:
+        GamePause();
+
+        void init() override;
+        bool update(double dt) override;
+        void onEnd() override;
+    };
+}
