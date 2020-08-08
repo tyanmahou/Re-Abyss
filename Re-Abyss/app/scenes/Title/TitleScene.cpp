020e7521669ced286ef447b6961d255070317adb
tyanmahou <tyanmahou@gmail.com>
2020-08-08 21:39:41 +0900

    add title scene

---
+++ Re-Abyss/app/scenes/Title/TitleScene.cpp
@@ -0,0 +1,15 @@
+#include "TitleScene.hpp"
+
+namespace abyss
+{
+    TitleScene::TitleScene(const InitData& init) :
+        ISceneBase(init)
+    {}
+
+    void TitleScene::update()
+    {}
+
+    void TitleScene::draw() const
+    {}
+
+}
