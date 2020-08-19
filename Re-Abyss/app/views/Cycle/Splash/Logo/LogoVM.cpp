c8fbcedb93d63f71ed2b1adb6444e5b1b57f9ee1
tyanmahou <tyanmahou@gmail.com>
2020-08-20 00:42:43 +0900

    add logo vm

---
+++ Re-Abyss/app/views/Cycle/Splash/Logo/LogoVM.cpp
@@ -0,0 +1,17 @@
+#include "LogoVM.hpp"
+#include <abyss/commons/ResourceManager/ResourceManager.hpp>
+#include <Siv3D.hpp>
+
+namespace abyss::cycle::splash
+{
+    LogoVM::LogoVM():
+        m_texture(ResourceManager::Main()->loadTexture(U"cycle/splash/logo.png"))
+    {}
+    void LogoVM::draw() const
+    {
+        const auto alpha = Math::Saturate(Periodic::Triangle0_1(1s, m_time0_1) * (1 / 0.75));
+
+        m_texture.drawAt(m_pos, ColorF(1.0, alpha));
+    }
+
+}
