b022615a3bdefb27e02b53a67469ffe817038d27
tyanmahou <tyanmahou@gmail.com>
2020-10-12 00:06:41 +0900

    add sqlite3 link

--- Re-Abyss/app/StaticLibs.hpp
+++ Re-Abyss/app/StaticLibs.hpp
@@ -1,12 +1,14 @@
-﻿#pragma once
+#pragma once
 
 #include <Siv3D/Platform.hpp>
 
 #if SIV3D_PLATFORM(WINDOWS)
 #if SIV3D_BUILD_TYPE(DEBUG)
 #pragma comment(lib, "S3DTiled_d")
+#pragma comment(lib, "SQLite-for-Siv3D_d")
 #else
 #pragma comment(lib, "S3DTiled")
+#pragma comment(lib, "SQLite-for-Siv3D")
 #endif
 #endif
 
