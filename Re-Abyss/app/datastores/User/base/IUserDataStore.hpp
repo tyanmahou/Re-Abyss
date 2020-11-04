23e3eb53eda8df90fd1767b86850bd19ead1181d
tyanmahou <tyanmahou@gmail.com>
2020-11-04 21:33:00 +0900

    update datastore

---
+++ Re-Abyss/app/datastores/User/base/IUserDataStore.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <abyss/entities/User/UserEntity.hpp>
+#include <Siv3D/Array.hpp>
+#include <Siv3D/Optional.hpp>
+
+namespace abyss::User
+{
+    class IUserDataStore
+    {
+    public:
+        virtual ~IUserDataStore() = default;
+
+        virtual s3d::Array<UserEntity> selectAll()const = 0;
+        virtual s3d::Optional<UserEntity> select(s3d::int32 userId)const = 0;
+
+        virtual void update(const UserEntity& entity)const = 0;
+    };
+}
