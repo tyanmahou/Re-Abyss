f310b8cb50fd92dbfa0baa5e656f6bb1c0fc776a
tyanmahou <tyanmahou@gmail.com>
2020-12-15 23:53:24 +0900

    update ref

--- Re-Abyss/app/utils/Ref/Ref.hpp
+++ Re-Abyss/app/utils/Ref/Ref.hpp
@@ -30,7 +30,9 @@ namespace abyss
         }
         [[nodiscard]] T* get() const noexcept
         {
-            return m_ptr.lock().get();
+            auto  lock = m_ptr.lock();
+            auto p = lock.get();
+            return p;
         }
 
         [[nodiscard]] explicit operator bool()const noexcept
@@ -43,9 +45,44 @@ namespace abyss
         }
     };
 
+    template<>
+    class Ref<void>
+    {
+        std::weak_ptr<void> m_ptr;
+    public:
+        Ref() = default;
+        Ref(const std::shared_ptr<void>& sharedPtr) :
+            m_ptr(sharedPtr)
+        {}
+        Ref(std::nullptr_t) :
+            m_ptr(std::weak_ptr<void>())
+        {}
+
+        [[nodiscard]] void* get() const noexcept
+        {
+            auto  lock = m_ptr.lock();
+            auto p = lock.get();
+            return p;
+        }
+
+        [[nodiscard]] explicit operator bool()const noexcept
+        {
+            return !m_ptr.expired();
+        }
+        [[nodiscard]] std::shared_ptr<void> lock() const
+        {
+            return m_ptr.lock();
+        }
+    };
     template<class T, class U>
     Ref<T> RefCast(const Ref<U>& ref)
     {
         return Ref<T>(std::dynamic_pointer_cast<T>(ref.lock()));
+    }
+
+    template<class T, class U>
+    Ref<T> StaticRefCast(const Ref<U>& ref)
+    {
+        return Ref<T>(std::static_pointer_cast<T>(std::static_pointer_cast<void>(ref.lock())));
     }
 }
