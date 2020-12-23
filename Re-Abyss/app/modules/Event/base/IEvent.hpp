533abda4f83e49b0d9893c9650aa92609c511a3d
tyanmahou <tyanmahou@gmail.com>
2020-12-24 00:22:51 +0900

    extends game object

--- Re-Abyss/app/modules/Event/base/IEvent.hpp
+++ Re-Abyss/app/modules/Event/base/IEvent.hpp
@@ -1,9 +1,10 @@
-﻿#pragma once
+#pragma once
 #include <abyss/commons/Fwd.hpp>
+#include <abyss/modules/GameObject/GameObject.hpp>
 
 namespace abyss::Event
 {
-    class IEvent
+    class IEvent : public GameObject
     {
     protected:
         Manager* m_pManager = nullptr;
@@ -15,8 +16,13 @@ namespace abyss::Event
             m_pManager = manager;
         }
         virtual void onStart(){}
-        virtual bool update(double dt) = 0;
+        virtual bool update([[maybe_unused]] double dt) { return true; }
         virtual void onEnd(){}
+
+        void setIsWorldStop(bool isWorldStop)
+        {
+            m_isWorldStop = isWorldStop;
+        }
 
         bool isWorldStop() const
         {
