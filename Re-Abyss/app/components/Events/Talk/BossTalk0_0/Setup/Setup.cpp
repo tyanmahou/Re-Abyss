edfc0436214ac3b999888b6a24dd70572bca49f1
tyanmahou <tyanmahou@gmail.com>
2020-12-27 23:38:31 +0900

    fix setup

--- Re-Abyss/app/components/Events/Talk/BossTalk0_0/Setup/Setup.cpp
+++ Re-Abyss/app/components/Events/Talk/BossTalk0_0/Setup/Setup.cpp
@@ -1,28 +1,33 @@
-//#include "Setup.hpp"
-//#include <abyss/modules/System/System.hpp>
-//#include <abyss/components/Actors/Enemy/CodeZero/CodeZeroProxy.hpp>
-//#include <abyss/components/Actors/Enemy/CodeZero/Demo/Builder.hpp>
-//#include <abyss/modules/Manager/Manager.hpp>
-//#include <abyss/modules/UI/UI.hpp>
-//
-//namespace abyss::Event::Talk::BossTalk0_0
-//{
-//    void Setup::onStart()
-//    {
-//        m_pManager->getModule<UI>()->setActiveAll(false);
-//        auto world = m_pManager->getModule<World>();
-//        
-//        if (auto codeZero = world->find<Actor::Enemy::CodeZero::CodeZeroProxy>()) {
-//            world->create<Actor::Enemy::CodeZero::Demo::Builder>(codeZero->getPos());
-//            codeZero->setActive(false);
-//        }
-//
-//        world->flush();
-//    }
-//
-//    bool Setup::update([[maybe_unused]]double dt)
-//    {
-//        return false;
-//    }
-//
-//}
+#include "Setup.hpp"
+
+#include <abyss/modules/Event/Talk/TalkObj.hpp>
+#include <abyss/modules/World/World.hpp>
+#include <abyss/modules/UI/UI.hpp>
+
+#include <abyss/components/Actors/Enemy/CodeZero/CodeZeroProxy.hpp>
+#include <abyss/components/Actors/Enemy/CodeZero/Demo/Builder.hpp>
+
+namespace abyss::Event::Talk::BossTalk0_0
+{
+    Setup::Setup(TalkObj* pTalk):
+        m_pTalk(pTalk)
+    {}
+
+    void Setup::onStart()
+    {
+        m_pTalk->getModule<UI>()->setActiveAll(false);
+        auto world = m_pTalk->getModule<World>();
+        
+        if (auto codeZero = world->find<Actor::Enemy::CodeZero::CodeZeroProxy>()) {
+            world->create<Actor::Enemy::CodeZero::Demo::Builder>(codeZero->getPos());
+            codeZero->setActive(false);
+        }
+
+        world->flush();
+    }
+
+    Coro::Task<> Setup::onTalk()
+    {
+        co_return;
+    }
+}
--- Re-Abyss/app/components/Events/Talk/BossTalk0_0/Setup/Setup.hpp
+++ Re-Abyss/app/components/Events/Talk/BossTalk0_0/Setup/Setup.hpp
@@ -1,11 +1,21 @@
-//#pragma once
-//#include <abyss/modules/Event/base/IEvent.hpp>
-//
-//namespace abyss::Event::Talk::BossTalk0_0
-//{
-//    class Setup : public IEvent
-//    {
-//        void onStart()override;
-//        bool update(double dt) override;
-//    };
-//}
+#pragma once
+#include <abyss/components/base/IComponent.hpp>
+#include <abyss/components/Events/Talk/base/ITalker.hpp>
+
+namespace abyss::Event::Talk::BossTalk0_0
+{
+    class Setup : 
+        public IComponent,
+        public ITalker
+    {
+    public:
+        Setup(TalkObj* pTalk);
+
+        void onStart() override;
+
+        Coro::Task<> onTalk() override;
+
+    private:
+        TalkObj* m_pTalk;
+    };
+}
