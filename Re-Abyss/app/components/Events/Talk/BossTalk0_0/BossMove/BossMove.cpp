362612adfca02eaee7556c29a490f3f16b190093
tyanmahou <tyanmahou@gmail.com>
2020-12-28 00:12:37 +0900

    fix bossmove

--- Re-Abyss/app/components/Events/Talk/BossTalk0_0/BossMove/BossMove.cpp
+++ Re-Abyss/app/components/Events/Talk/BossTalk0_0/BossMove/BossMove.cpp
@@ -1,52 +1,62 @@
-//#include "BossMove.hpp"
-//#include <abyss/modules/System/System.hpp>
-//#include <abyss/modules/Camera/Camera.hpp>
-//#include <abyss/models/Camera/QuakeModel.hpp>
-//#include <abyss/components/Actors/Enemy/CodeZero/Demo/DemoCtrl.hpp>
-//
-//namespace abyss::Event::Talk::BossTalk0_0
-//{
-//    void BossMove::onStart()
-//    {
-//        auto world = m_pManager->getModule<World>();
-//        m_codeZeroDemo = world->find<Actor::Enemy::CodeZero::Demo::DemoCtrl>();
-//        m_initPos = m_codeZeroDemo->getPos();
-//
-//        m_targetPos = m_initPos - s3d::Vec2{ 0, 400 };
-//
-//        m_quake = m_pManager->getModule<Camera>()->startQuake(2.0);
-//    }
-//
-//    bool BossMove::update(double dt)
-//    {
-//        if (!m_codeZeroDemo) {
-//            return false;
-//        }
-//        if (m_codeZeroDemo->moveToTarget(dt)) {
-//            return true;
-//        }
-//        auto camera = m_pManager->getModule<Camera>();
-//        if (!m_isRequestedLastQuake) {
-//            if (m_quake) {
-//                m_quake->stop();
-//            }
-//            m_lastQuake = camera->startQuake(10, 0.3);
-//            m_isRequestedLastQuake = true;
-//        }
-//        return m_lastQuake && !m_lastQuake->isEnd();
-//    }
-//
-//    void BossMove::onEnd()
-//    {
-//        if (m_quake) {
-//            m_quake->stop();
-//        }
-//        if (m_lastQuake) {
-//            m_lastQuake->stop();
-//        }
-//        if (!m_codeZeroDemo) {
-//            return;
-//        }
-//        m_codeZeroDemo->setPos(m_targetPos);
-//    }
-//}
+#include "BossMove.hpp"
+
+#include <abyss/modules/GlobalTime/GlobalTime.hpp>
+#include <abyss/modules/World/World.hpp>
+#include <abyss/modules/Camera/Camera.hpp>
+#include <abyss/modules/Event/Talk/TalkObj.hpp>
+#include <abyss/models/Camera/QuakeModel.hpp>
+#include <abyss/components/Actors/Enemy/CodeZero/Demo/DemoCtrl.hpp>
+
+namespace abyss::Event::Talk::BossTalk0_0
+{
+    BossMove::BossMove(TalkObj* pTalk) :
+        m_pTalk(pTalk)
+    {}
+
+    void BossMove::onStart()
+    {
+        auto world = m_pTalk->getModule<World>();
+        m_codeZeroDemo = world->find<Actor::Enemy::CodeZero::Demo::DemoCtrl>();
+        m_initPos = m_codeZeroDemo->getPos();
+
+        m_targetPos = m_initPos - s3d::Vec2{ 0, 400 };
+
+        m_quake = m_pTalk->getModule<Camera>()->startQuake(2.0);
+    }
+
+    Coro::Task<> BossMove::onTalk()
+    {
+        if (!m_codeZeroDemo) {
+            co_return;
+        }
+        auto globalTime = m_pTalk->getModule<GlobalTime>();
+        // 移動
+        while (m_codeZeroDemo->moveToTarget(globalTime->deltaTime())) {
+            co_yield{};
+        }
+
+        if (m_quake) {
+            m_quake->stop();
+        }
+        auto camera = m_pTalk->getModule<Camera>();
+        m_lastQuake = camera->startQuake(10, 0.3);
+
+        while (!m_lastQuake->isEnd()) {
+            co_yield{};
+        }
+    }
+
+    void BossMove::onEnd()
+    {
+        if (m_quake) {
+            m_quake->stop();
+        }
+        if (m_lastQuake) {
+            m_lastQuake->stop();
+        }
+        if (!m_codeZeroDemo) {
+            return;
+        }
+        m_codeZeroDemo->setPos(m_targetPos);
+    }
+}
--- Re-Abyss/app/components/Events/Talk/BossTalk0_0/BossMove/BossMove.hpp
+++ Re-Abyss/app/components/Events/Talk/BossTalk0_0/BossMove/BossMove.hpp
@@ -1,28 +1,32 @@
-//#pragma once
-//#include <abyss/utils/Ref/Ref.hpp>
-//#include <abyss/modules/Event/base/IEvent.hpp>
-//
-//namespace abyss::Actor::Enemy::CodeZero::Demo
-//{
-//    class DemoCtrl;
-//}
-//namespace abyss::Event::Talk::BossTalk0_0
-//{
-//    class BossMove : public IEvent
-//    {
-//    public:
-//        void onStart()override;
-//        bool update(double dt) override;
-//        void onEnd() override;
-//    private:
-//        Ref<Actor::Enemy::CodeZero::Demo::DemoCtrl> m_codeZeroDemo;
-//
-//        s3d::Vec2 m_initPos;
-//        s3d::Vec2 m_targetPos;
-//        bool m_isRequestedLastQuake = false;
-//
-//        Ref<QuakeModel> m_quake;
-//        Ref<QuakeModel> m_lastQuake;
-//    public:
-//    };
-//}
+#pragma once
+#include <abyss/utils/Ref/Ref.hpp>
+#include <abyss/components/base/IComponent.hpp>
+#include <abyss/components/Events/Talk/base/ITalker.hpp>
+
+namespace abyss::Actor::Enemy::CodeZero::Demo
+{
+    class DemoCtrl;
+}
+namespace abyss::Event::Talk::BossTalk0_0
+{
+    class BossMove :
+        public IComponent,
+        public ITalker
+    {
+    public:
+        BossMove(TalkObj* pTalk);
+
+        void onStart()override;
+        Coro::Task<> onTalk() override;
+        void onEnd() override;
+    private:
+        TalkObj* m_pTalk;
+        Ref<Actor::Enemy::CodeZero::Demo::DemoCtrl> m_codeZeroDemo;
+
+        s3d::Vec2 m_initPos;
+        s3d::Vec2 m_targetPos;
+
+        Ref<QuakeModel> m_quake;
+        Ref<QuakeModel> m_lastQuake;
+    };
+}
