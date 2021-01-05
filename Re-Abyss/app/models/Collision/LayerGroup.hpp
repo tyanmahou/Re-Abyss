7227538d0151b0bda334156a3fff77c76770fabd
tyanmahou <tyanmahou@gmail.com>
2021-01-06 00:16:15 +0900

    fix constexper

--- Re-Abyss/app/models/Collision/LayerGroup.hpp
+++ Re-Abyss/app/models/Collision/LayerGroup.hpp
@@ -3,9 +3,9 @@
 
 namespace abyss::LayerGroup
 {
-    inline static s3d::int32 Player = 0x01;
-    inline static s3d::int32 Enemy = 0x02;
-    inline static s3d::int32 Gimmick = 0x04;
-    inline static s3d::int32 Item = 0x08;
-    inline static s3d::int32 Map = 0x0F;
+    inline constexpr s3d::int32 Player = 0x01;
+    inline constexpr s3d::int32 Enemy = 0x02;
+    inline constexpr s3d::int32 Gimmick = 0x04;
+    inline constexpr s3d::int32 Item = 0x08;
+    inline constexpr s3d::int32 Map = 0x0f;
 }
