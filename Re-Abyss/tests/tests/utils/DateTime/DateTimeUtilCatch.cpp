cfa2da45c9049df99a7a8b86daf5d6b9da3aee11
tyanmahou <tyanmahou@gmail.com>
2021-01-31 16:26:42 +0900

    add test

--- Re-Abyss/tests/tests/utils/DateTime/DateTimeUtilCatch.cpp
+++ Re-Abyss/tests/tests/utils/DateTime/DateTimeUtilCatch.cpp
@@ -30,6 +30,17 @@ namespace abyss::tests
             auto d = DateTimeUtil::ToTimeDuration(U"12:01:02");
             REQUIRE(s3d::FormatTime(d, U"HH:mm:ss") == U"12:01:02");
         }
+
+        SECTION("test datetime 2038")
+        {
+            s3d::DateTime dt = DateTimeUtil::ToDateTime(U"2040-01-08 12:01:02");
+            REQUIRE(dt.year == 2040);
+            REQUIRE(dt.month == 1);
+            REQUIRE(dt.day == 8);
+            REQUIRE(dt.hour == 12);
+            REQUIRE(dt.minute == 1);
+            REQUIRE(dt.second == 2);
+        }
     }
 }
 #endif
