bb8b01a1000ca32adcec07cc58097933c630e389
tyanmahou <tyanmahou@gmail.com>
2021-04-03 04:54:31 +0900

    add decor group

--- Re-Abyss/app/entities/Decor/DecorEntity.hpp
+++ Re-Abyss/app/entities/Decor/DecorEntity.hpp
@@ -1,33 +1,9 @@
 #pragma once
-#include <variant>
 #include <Siv3D/Vector2D.hpp>
+#include <abyss/entities/Decor/Motif.hpp>
 
 namespace abyss::decor
 {
-	namespace Motif
-	{
-		enum class Common
-		{
-			Common,
-		};
-
-		enum class City
-		{
-			StreetLight,
-		};
-
-		enum class Gimmick
-		{
-			Door,
-		};
-
-		enum class Map
-		{
-			Common,
-		};
-	}
-
-	using DecorType = std::variant <Motif::Common, Motif::City, Motif::Gimmick, Motif::Map>;
 
 	struct DecorEntity
 	{
---
+++ Re-Abyss/app/entities/Decor/DecorGroup.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace abyss::decor
+{
+	enum class DecorGroup
+	{
+		Front,
+		Back,
+	};
+}
