0914ba4244a77efff039e1761ee749eb668b52b9
tyanmahou <tyanmahou@gmail.com>
2021-04-04 22:24:17 +0900

    update datastore

---
+++ Re-Abyss/app/datastores/Decor/TmxDecorGraphicsDataStore.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <abyss/commons/Fwd.hpp>
+#include <abyss/entities/Decor/DecorEntity.hpp>
+#include <abyss/entities/Decor/DecorGroup.hpp>
+#include <Siv3D/Array.hpp>
+
+namespace abyss::decor
+{
+    class IDecorDataStore
+    {
+    public:
+        virtual ~IDecorDataStore() = default;
+
+        virtual s3d::Array<DecorEntity> select(DecorGroup group)const = 0;
+    };
+}
---
+++ Re-Abyss/app/datastores/Decor/base/IDecorDataStore.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <abyss/commons/Fwd.hpp>
+#include <abyss/entities/Decor/DecorEntity.hpp>
+#include <abyss/entities/Decor/DecorGroup.hpp>
+#include <Siv3D/Array.hpp>
+
+namespace abyss::decor
+{
+    class IDecorDataStore
+    {
+    public:
+        virtual ~IDecorDataStore() = default;
+
+        virtual s3d::Array<DecorEntity> select(DecorGroup group) const = 0;
+    };
