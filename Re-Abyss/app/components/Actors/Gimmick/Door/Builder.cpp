8ae5986c1a84fee49a5c5fa2c6cc1f3848996478
tyanmahou <tyanmahou@gmail.com>
2021-04-20 21:43:11 +0900

    rebase delete door view

--- Re-Abyss/app/components/Actors/Gimmick/Door/Builder.cpp
+++ Re-Abyss/app/components/Actors/Gimmick/Door/Builder.cpp
@@ -1,8 +1,16 @@
 #include "Builder.hpp"
 
 #include <abyss/components/Actors/Commons/CustomCollider.hpp>
+#include <abyss/components/Actors/Commons/CustomDraw.hpp>
 #include <abyss/components/Actors/Gimmick/Door/DoorProxy.hpp>
 #include <abyss/components/Actors/Gimmick/Door/GimmickReacter.hpp>
+
+#include <abyss/views/actors/Gimmick/Door/DoorVM.hpp>
+
+namespace
+{
+	class Drawer;
+}
 
 namespace abyss::Actor::Gimmick::Door
 {
@@ -21,5 +29,45 @@ namespace abyss::Actor::Gimmick::Door
 		{
 			pActor->attach<GimmickReacter>(pActor);
 		}
+
+		// 描画制御
+		{
+			pActor->attach<CustomDraw>()
+				->setDrawer<Drawer>(pActor);
+		}
     }
+}
+
+namespace
+{
+	using namespace abyss;
+	using namespace abyss::Actor;
+	using namespace abyss::Actor::Gimmick::Door;
+
+	class Drawer : public CustomDraw::IImpl
+	{
+	public:
+		Drawer(IActor* pActor) :
+			m_pActor(pActor),
+			m_view(std::make_unique<DoorVM>())
+		{}
+
+		void onStart() override
+		{
+			m_door = m_pActor->find<DoorProxy>();
+		}
+		void onDraw() const override
+		{
+			m_view
+				->setPos(m_door->getPos())
+				.setKind(m_door->getKind())
+				.setTime(m_pActor->getDrawTimeSec())
+				.draw();
+		}
+	private:
+		IActor* m_pActor;
+		Ref<DoorProxy> m_door;
+
+		std::unique_ptr<DoorVM> m_view;
+	};
 }
