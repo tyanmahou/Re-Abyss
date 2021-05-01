d4c81989229517c484a7e25eb62eac9173302ee2
tyanmahou <tyanmahou@gmail.com>
2021-05-01 21:50:18 +0900

    add clear

--- Re-Abyss/app/modules/DrawManager/DrawManager.cpp
+++ Re-Abyss/app/modules/DrawManager/DrawManager.cpp
@@ -2,6 +2,12 @@
 
 namespace abyss
 {
+    void DrawManager::clear()
+    {
+        for (auto& drawers : m_drawers) {
+            drawers.clear();
+        }
+    }
     void DrawManager::add(DrawLayer layer, std::function<void()> drawer)
     {
         m_drawers[static_cast<size_t>(layer)].push_back(drawer);
--- Re-Abyss/app/modules/DrawManager/DrawManager.hpp
+++ Re-Abyss/app/modules/DrawManager/DrawManager.hpp
@@ -9,6 +9,7 @@ namespace abyss
     class DrawManager
     {
     public:
+        void clear();
         void add(DrawLayer layer, std::function<void()> drawer);
     private:
         std::array<s3d::Array<std::function<void()>>, DrawLayerSize> m_drawers;
