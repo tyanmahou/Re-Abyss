f57d7555cd0b4a270d6e890f13ad7126f015b806
tyanmahou <tyanmahou@gmail.com>
2021-06-22 23:31:32 +0900

    update

--- Re-Abyss/app/views/Actor/Item/Recovery/RecoveryEffect.cpp
+++ Re-Abyss/app/views/Actor/Item/Recovery/RecoveryEffect.cpp
@@ -1,5 +1,8 @@
 #include "RecoveryEffect.hpp"
 #include <Siv3D.hpp>
+#include <abyss/utils/Coro/Task/Task.hpp>
+#include <abyss/utils/Coro/Wait/Wait.hpp>
+#include <memory>
 
 namespace
 {
@@ -14,7 +17,7 @@ namespace abyss::Actor::Item::Recovery
         {
         public:
             Particle(bool isFrame = false) :
-                m_localPos(RandomVec2(Circle(40.0))),
+                m_localPos(RandomVec2(Circle(Vec2{0, 15}, 40.0))),
                 m_size(Random(4.0, 10.0)),
                 m_angle(Random()* Math::TwoPi),
                 m_isFrame(isFrame)
@@ -95,6 +98,61 @@ namespace abyss::Actor::Item::Recovery
         private:
             double m_startAngle = 0.0;
         };
+        struct Flashs
+        {
+            struct Flash
+            {
+            public:
+                Flash()
+                {
+                    m_localPos = RandomBool() ?
+                        RandomVec2(Rect{-30, 0, 15, 30}) :
+                        RandomVec2(Rect{ 15, 0, 15, 30});
+                }
+                bool update(const Vec2& pos, double dt)
+                {
+                    m_lifeTime += dt;
+                    auto rate = m_lifeTime / 0.2;
+                    auto alpha = (0.2 - m_lifeTime) / m_lifeTime;
+                    Line(pos + m_localPos + Vec2{ 0, -5 } * rate, pos + m_localPos + Vec2{0, -50} * rate).draw(2.0, ColorF(1.0, alpha));
+                    return m_lifeTime <= 0.2;
+                }
+            private:
+                Vec2 m_localPos;
+                double m_lifeTime = 0.0;
+            };
+            Coro::Task<void> task()
+            {
+                auto clock = [this] {return Clock::FromSec(m_time); };
+                for ([[maybe_unused]]auto count : step(1, 10)) {
+                    m_flashs.emplace_back();
+                    co_yield Coro::WaitForSecondsEx(0.1s, clock);
+                }
+            }
+        public:
+            Flashs():
+                m_task(std::make_unique<Coro::Task<void>>(this->task()))
+            {}
+            bool update(const Vec2& pos, double t)
+            {
+                double dt = t - m_time;
+                m_time = t;
+
+                for (auto it = m_flashs.begin(); it != m_flashs.end();) {
+                    if (!it->update(pos, dt)) {
+                        it = m_flashs.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+
+                return m_task->moveNext();
+            }
+        private:
+            double m_time = 0;
+            std::unique_ptr<Coro::Task<void>> m_task;
+            s3d::Array<Flash> m_flashs;
+        };
     public:
         Impl(const Ref<ILocator>& locator) :
             m_locator(locator)
@@ -116,6 +174,7 @@ namespace abyss::Actor::Item::Recovery
             ScopedRenderStates2D blend(BlendState::Additive);
 
             bool result = m_circles.update(m_pos, t);
+            result |= m_flashs.update(m_pos, t);
             for (auto&& particle : m_particles) {
                 result |= particle.update(m_pos, t);
             }
@@ -126,6 +185,7 @@ namespace abyss::Actor::Item::Recovery
         s3d::Vec2 m_pos;
 
         Circles m_circles;
+        Flashs m_flashs;
         s3d::Array<Particle> m_particles;
     };
     RecoveryEffect::RecoveryEffect(const Ref<ILocator>& locator) :
