f938f8c1f1abe25c407997849e1d8b8a6bfab36e
tyanmahou <tyanmahou@gmail.com>
2021-08-02 20:55:39 +0900

    fix resolution

--- Re-Abyss/app/commons/Constants.hpp
+++ Re-Abyss/app/commons/Constants.hpp
@@ -11,14 +11,14 @@ namespace abyss::Constants
     inline constexpr auto* const AppName = U"Abyss Dream";
     // 解像度
     template<class Type>
-    inline constexpr s3d::Vector2D<Type> AppResolution_v{ 960, 540 };
+    inline constexpr s3d::Vector2D<Type> AppResolution_v{ 1120, 630 };
     inline constexpr s3d::Size AppResolution = AppResolution_v<s3d::int32>.asPoint();
     inline constexpr s3d::Vec2 AppResolutionF = AppResolution_v<double>;
 
 	template<class Type>
-	inline constexpr s3d::Vector2D<Type> GameScreenSize_v{960, 520};
+	inline constexpr s3d::Vector2D<Type> GameScreenSize_v{1120, 640};
 	template<class Type>
-	inline constexpr s3d::Vector2D<Type> GameScreenOffset_v{0, 20};
+	inline constexpr s3d::Vector2D<Type> GameScreenOffset_v{0, -10};
 
     inline constexpr s3d::Vec2 GameScreenSize = GameScreenSize_v<double>;
     inline constexpr s3d::Vec2 GameScreenOffset = GameScreenOffset_v<double>;
