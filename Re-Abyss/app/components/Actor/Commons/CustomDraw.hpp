4ee803ce40e356c207d7646fc2ad41f32d0205c4
tyanmahou <tyanmahou@gmail.com>
2021-09-06 23:43:50 +0900

    refactor custom draw

--- Re-Abyss/app/components/Actor/Commons/CustomDraw.hpp
+++ Re-Abyss/app/components/Actor/Commons/CustomDraw.hpp
@@ -1,8 +1,5 @@
 #pragma once
-#include <memory>
-#include <functional>
-#include <Siv3D/String.hpp>
-#include <abyss/components/base/IComponent.hpp>
+#include <abyss/components/Common/CustomDrawBase.hpp>
 #include <abyss/components/Actor/base/IDraw.hpp>
 
 namespace abyss::Actor
@@ -10,45 +7,5 @@ namespace abyss::Actor
     /// <summary>
     /// 汎用描画
     /// </summary>
-    class CustomDraw :
-        public IComponent,
-        public IDraw
-    {
-    public:
-        class IImpl
-        {
-        public:
-            virtual ~IImpl() = default;
-
-            virtual void setup([[maybe_unused]]Executer executer) {}
-            virtual void onStart() = 0;
-            virtual void onDraw()const = 0;
-        };
-    private:
-        std::unique_ptr<IImpl> m_pImpl;
-    public:
-        void setup(Executer executer) override;
-
-        void onStart() override;
-
-        void onDraw() const override;
-
-        CustomDraw& setDrawer(std::unique_ptr<IImpl>&& drawer);
-
-        template<class Impl, class... Args>
-        CustomDraw& setDrawer(Args&&... args)
-        {
-            return setDrawer(std::make_unique<Impl>(std::forward<Args>(args)...));
-        }
-        CustomDraw& setDrawer(const std::function<void()>& drawer);
-    };
-}
-
-namespace abyss
-{
-    template<>
-    struct ComponentTree<Actor::CustomDraw>
-    {
-        using Base = Actor::IDraw;
-    };
+    using CustomDraw = CustomDrawBase<IDraw>;
 }
