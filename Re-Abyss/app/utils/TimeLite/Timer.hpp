f4dd3e537514202dc51ac5462e52461ce81c4c3c
tyanmahou <tyanmahou@gmail.com>
2021-09-12 23:59:22 +0900

    add timer

---
+++ Re-Abyss/app/utils/TimeLite/Timer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <Siv3D/Duration.hpp>
+
+namespace abyss::TimeLite
+{
+    class Timer
+    {
+    public:
+        Timer(double time = 0):
+            m_total(time)
+        {}
+
+        double update(double dt)
+        {
+            m_current += dt;
+            if (m_current >= m_total) {
+                auto tmp = m_current;
+                m_current = m_total;
+                return tmp - m_total;
+            }
+            return 0.0;
+        }
+
+        bool isEnd() const
+        {
+            return m_current >= m_total;
+        }
+    private:
+        double m_current = 0;
+        double m_total = 0;
+    };
+}
