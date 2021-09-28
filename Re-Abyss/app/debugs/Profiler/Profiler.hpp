4318df84a413635ea80aca5295f9e1ca5d926e8e
tyanmahou <tyanmahou@gmail.com>
2021-09-28 23:28:45 +0900

    update profiler

--- Re-Abyss/app/debugs/Profiler/Profiler.hpp
+++ Re-Abyss/app/debugs/Profiler/Profiler.hpp
@@ -25,10 +25,21 @@ namespace abyss::Debug
         std::unique_ptr<Impl> m_pImpl;
     };
 
-    namespace Prof
+    namespace ProfKind
     {
-        inline static constexpr s3d::StringView UpdateAll = U"Update All";
-        inline static constexpr s3d::StringView DrawAll = U"Draw All";
+        inline static constexpr s3d::StringView UpdateAll       = U"Update All";
+        inline static constexpr s3d::StringView DrawAll         = U"Draw All";
+        inline static constexpr s3d::StringView DrawReuqest     = U"  Draw Reuqest";
+        inline static constexpr s3d::StringView DrawBackGround  = U"  Draw BackGround";
+        inline static constexpr s3d::StringView DrawDecorBack   = U"  Draw DecorBack";
+        inline static constexpr s3d::StringView DrawDecorMiddle = U"  Draw DecorMiddle";
+        inline static constexpr s3d::StringView DrawWorld       = U"  Draw World";
+        inline static constexpr s3d::StringView DrawDecorFront  = U"  Draw DecorFront";
+        inline static constexpr s3d::StringView DrawLight       = U"  Draw Light";
+        inline static constexpr s3d::StringView DrawDist        = U"  Draw Dist";
+        inline static constexpr s3d::StringView DrawDebug       = U"  Draw Debug";
+        inline static constexpr s3d::StringView DrawPostEffect  = U"  Draw PostEffect";
+        inline static constexpr s3d::StringView DrawUI          = U"  Draw UI";
     }
 }
 
