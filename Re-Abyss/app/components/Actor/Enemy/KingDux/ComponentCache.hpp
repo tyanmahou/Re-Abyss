a7cd93fe8a6ab0ff481fc6e9cb095d4be8357bd8
tyanmahou <tyanmahou@gmail.com>
2021-11-28 23:53:10 +0900

    add component cache empty

---
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/ComponentCache.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <abyss/modules/Actor/base/ActorObj.hpp>
+
+namespace abyss::Actor::Enemy::KingDux
+{
+    class ComponentCache
+    {
+    protected:
+    public:
+        void onCache(ActorObj* pActor)
+        {
+        }
+    };
+}
