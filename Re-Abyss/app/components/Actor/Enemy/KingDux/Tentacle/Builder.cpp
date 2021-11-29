5d0b5e0168ff197628f7f94038f475248e243a80
tyanmahou <tyanmahou@gmail.com>
2021-11-29 23:57:49 +0900

    add tentacle builder

---
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/Builder.cpp
@@ -0,0 +1,18 @@
+#include "Builder.hpp"
+#include <abyss/modules/Actor/base/ActorObj.hpp>
+
+namespace
+{
+    class ViewBinder;
+}
+
+namespace abyss::Actor::Enemy::KingDux::Tentacle
+{
+    void Builder::Build(ActorObj* pActor)
+    {
+    }
+}
+
+namespace
+{
+}
---
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/Builder.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <abyss/commons/Fwd.hpp>
+#include <abyss/entities/Actor/Enemy/KingDuxEntity.hpp>
+
+namespace abyss::Actor::Enemy::KingDux::Tentacle
+{
+    struct Builder
+    {
+        static void Build(ActorObj* pActor);
+    };
+}
