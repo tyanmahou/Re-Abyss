434bfde85e2396b8c906fcbf60eeaacf08441e8a
tyanmahou <tyanmahou@gmail.com>
2021-12-07 23:56:10 +0900

    update rotate

--- Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/State/RotateState.cpp
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/State/RotateState.cpp
@@ -1,4 +1,6 @@
 ﻿#include "RotateState.hpp"
+#include <abyss/components/Actor/utils/ActorUtils.hpp>
+#include <Siv3D.hpp>
 
 namespace abyss::Actor::Enemy::KingDux::Tentacle
 {
@@ -7,5 +9,15 @@ namespace abyss::Actor::Enemy::KingDux::Tentacle
 	}
 	void RotateState::update()
 	{
+        auto dt = m_pActor->deltaTime();
+
+        s3d::Vec2 d = ActorUtils::PlayerDiffVec(*m_pActor, *m_body);
+        const double speed = s3d::Math::ToRadians(60.0);
+        double rotate = m_rotate->getRotate();
+        if (m_rotate->getDir().cross(d) > 0) {
+            rotate += speed * dt;
+        } else {
+            rotate -= speed * dt;
+        }
 	}
 }
