fe346cb04c88a022f81f0c5527af39f72f78c9c4
tyanmahou <tyanmahou@gmail.com>
2021-12-13 23:57:20 +0900

    update stab

--- Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/State/StabState.cpp
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/Tentacle/State/StabState.cpp
@@ -1,5 +1,6 @@
 ﻿#include "StabState.hpp"
 #include <abyss/utils/Coro/Wait/Wait.hpp>
+#include <abyss/utils/TimeLite/Timer.hpp>
 
 namespace abyss::Actor::Enemy::KingDux::Tentacle
 {
@@ -12,6 +13,19 @@ namespace abyss::Actor::Enemy::KingDux::Tentacle
 	}
 	Coro::Task<> StabState::task()
 	{
+		TimeLite::Timer moveTimer{ 1.5 };
+		while (true) {
+			double dt = m_pActor->deltaTime();
+			moveTimer.update(dt);
+			m_body->setVelocity(m_rotate->getDir9() * 400.0);
+			if (moveTimer.isEnd()) {
+				break;
+			}
+			co_yield{};
+		}
+		m_body
+			->noneResistanced()
+			.setVelocity(s3d::Vec2::Zero());
 		co_return;
 	}
 }
