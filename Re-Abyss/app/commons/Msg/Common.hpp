372282ab586e6b23bb68e69b781073ee8a467a43
tyanmahou <tyanmahou@gmail.com>
2022-01-28 00:54:46 +0900

    update msg

--- Re-Abyss/app/commons/Msg/Common.hpp
+++ Re-Abyss/app/commons/Msg/Common.hpp
@@ -6,12 +6,15 @@ namespace abyss::Msg
 {
 	struct Common
 	{
-		[[TOML_BIND(UserPlayMode::Normal)]]
-		[[TOML_BIND(UserPlayMode::Hard)]]
+		[[TOML_BIND(UserPlayMode)]]
 		struct UserPlayMode
 		{
+			[[TOML_BIND(Normal)]]
 			inline static s3d::String Normal{};
+			[[TOML_BIND(Hard)]]
 			inline static s3d::String Hard{};
 		};
+	private:
+		[[no_unique_address]] UserPlayMode UserPlayMode;
 	};
 }
--- Re-Abyss/app/commons/Resource/Preload/Message.cpp
+++ Re-Abyss/app/commons/Resource/Preload/Message.cpp
@@ -6,7 +6,7 @@ namespace abyss::Resource::Preload
 {
 	void LoadMessage(s3d::StringView lang, const Assets* resource)
 	{
-		auto prefix = s3d::Fmt(Path::MsgPath)(lang);
+		auto prefix = s3d::Fmt(Path::MsgPath)(lang);		
 		resource->loadToml<Msg::Common>(U"Common.toml", prefix);
 	}
 }
