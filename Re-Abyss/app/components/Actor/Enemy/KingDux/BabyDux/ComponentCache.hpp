eec04b9bd8059f7830e5331fdfc3f74b11fbfa67
tyanmahou <tyanmahou@gmail.com>
2022-02-11 21:22:28 +0900

    update comp cache

--- Re-Abyss/app/components/Actor/Enemy/KingDux/BabyDux/ComponentCache.hpp
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/BabyDux/ComponentCache.hpp
@@ -1,16 +1,19 @@
 #pragma once
 #include <abyss/modules/Actor/base/ActorObj.hpp>
 #include <abyss/components/Actor/Common/Body.hpp>
+#include <abyss/components/Actor/Enemy/KingDux/BabyDux/Main.hpp>
 
 namespace abyss::Actor::Enemy::KingDux::BabyDux
 {
     class ComponentCache
     {
     protected:
+        Main* m_main;
         Body* m_body;
     public:
         void onCache(ActorObj* pActor)
         {
+            m_main = pActor->find<Main>().get();
             m_body = pActor->find<Body>().get();
         }
     };
