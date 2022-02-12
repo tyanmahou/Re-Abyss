8c39d6c82795d723a085f4dda5703a718b4dc81f
tyanmahou <tyanmahou@gmail.com>
2022-02-12 15:25:49 +0900

    update appear move

--- Re-Abyss/app/components/Actor/Enemy/KingDux/BabyDux/State/AppearState.cpp
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/BabyDux/State/AppearState.cpp
@@ -1,4 +1,6 @@
 #include <abyss/components/Actor/Enemy/KingDux/BabyDux/State/AppearState.hpp>
+#include <abyss/utils/TimeLite/Timer.hpp>
+#include <Siv3D.hpp>
 
 namespace abyss::Actor::Enemy::KingDux::BabyDux
 {
@@ -8,6 +10,24 @@ namespace abyss::Actor::Enemy::KingDux::BabyDux
 	}
 	Task<> AppearState::task()
 	{
+		// 移動
+		{
+			// 目標の高さ
+			const double targetY = m_main->parentPos().y + 400;
+			const Vec2 initPos = m_body->getPos();
+			const Vec2 targetPos = Vec2{ initPos.x, targetY };
+
+			TimeLite::Timer timer(2.0);
+			while (!timer.isEnd())
+			{
+				timer.update(m_pActor->deltaTime());
+
+				auto rate = timer.rate();
+				auto newPos = s3d::Math::Lerp(initPos, targetPos, rate);
+				m_body->moveToPos(newPos, m_pActor->deltaTime());
+				co_yield{};
+			}
+		}
 		co_return;
 	}
 }
