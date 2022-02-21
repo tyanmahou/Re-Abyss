5ae4fbf4ecdfda4fec80640f0dacb5d769a0a07e
tyanmahou <tyanmahou@gmail.com>
2022-02-21 23:59:22 +0900

    wim face

--- Re-Abyss/app/views/Actor/Enemy/CodeZero/Head/HeadVM.cpp
+++ Re-Abyss/app/views/Actor/Enemy/CodeZero/Head/HeadVM.cpp
@@ -37,6 +37,12 @@ namespace abyss::Actor::Enemy::CodeZero::Head
             if (m_forward == Forward::None) {
                 return m_texture(U"head")(0, 0, 170, 170);
             }
+            if (m_forward == Forward::Up) {
+                return m_texture(U"head")(0, 170, 170, 170).mirrored(m_forward == Forward::Right);
+            } else if (m_forward == Forward::Down) {
+                return m_texture(U"head")(170, 170, 170, 170).mirrored(m_forward == Forward::Right);
+            }
+
             return m_texture(U"head")(170, 0, 170, 170).mirrored(m_forward == Forward::Right);
         }();
 
