382e073b53d7e7c20ba754d6a7e437af2cfc3724
tyanmahou <tyanmahou@gmail.com>
2022-02-28 20:12:42 +0900

    add eft

--- Re-Abyss/app/components/Actor/Enemy/KingDux/State/AngryState.cpp
+++ Re-Abyss/app/components/Actor/Enemy/KingDux/State/AngryState.cpp
@@ -1,6 +1,10 @@
 #include <abyss/components/Actor/Enemy/KingDux/State/AngryState.hpp>
 #include <abyss/components/Actor/Enemy/KingDux/State/WaitState.hpp>
 
+#include <abyss/modules/Effect/Effects.hpp>
+#include <abyss/components/Actor/utils/BehaviorUtil.hpp>
+#include <abyss/components/Effect/Misc/ShockWaveDist/Builder.hpp>
+
 #include <Siv3D.hpp>
 
 namespace abyss::Actor::Enemy::KingDux
@@ -13,6 +17,17 @@ namespace abyss::Actor::Enemy::KingDux
 	}
 	Coro::Task<> AngryState::task()
 	{
+		co_await BehaviorUtil::WaitForSeconds(m_pActor, 1.0);
+		double timeSec = 3.5;
+		m_pActor->getModule<Effects>()->createWorldFront<Effect::Misc::ShockWaveDist::Builder>(
+			m_body->getPos(),
+			900.0,
+			200.0,
+			timeSec
+			);
+
+		co_await BehaviorUtil::WaitForSeconds(m_pActor, timeSec);
+
 		this->changeState<WaitState>();
 		co_return;
 	}
