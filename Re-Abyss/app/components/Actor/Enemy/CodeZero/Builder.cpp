176d37539c2ca78d13cf1b7a5f801b368fe84f4a
tyanmahou <tyanmahou@gmail.com>
2022-04-01 00:50:25 +0900

    update fix

--- Re-Abyss/app/components/Actor/Enemy/CodeZero/Builder.cpp
+++ Re-Abyss/app/components/Actor/Enemy/CodeZero/Builder.cpp
@@ -201,7 +201,7 @@ namespace
             return &m_view
                 ->setPos(m_head->getPos())
                 .setColorMul(m_colorCtrl->colorMul(2))
-                .setColorAdd(m_colorCtrl->colorMul(1))
+                .setColorAdd(m_colorCtrl->colorAdd(1))
                 ;
         }
         void setup(Executer executer) final
