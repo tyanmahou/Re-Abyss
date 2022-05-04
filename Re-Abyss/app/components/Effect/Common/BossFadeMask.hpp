63a96cdcfbd0fe18ad6b561e695cd02570220a21
tyanmahou <tyanmahou@gmail.com>
2022-05-04 09:50:54 +0900

    wip BossFadeMask

--- Re-Abyss/app/components/Effect/Common/BossFadeMask.hpp
+++ Re-Abyss/app/components/Effect/Common/BossFadeMask.hpp
@@ -3,5 +3,17 @@
 
 namespace abyss::Effect
 {
+	class BossFadeMask final : public IComponent
+	{
 
+	};
+}
+
+namespace abyss
+{
+    template<>
+    struct ComponentTree<Effect::BossFadeMask>
+    {
+        //using Base = Effect::IDraw;
+    };
 }
