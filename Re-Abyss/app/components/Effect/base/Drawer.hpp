2ef1e03f5c43aed22e9fd5639a09fbbe44a4f4ca
tyanmahou <tyanmahou@gmail.com>
2022-05-05 12:16:20 +0900

    update Drawer

--- Re-Abyss/app/components/Effect/base/Drawer.hpp
+++ Re-Abyss/app/components/Effect/base/Drawer.hpp
@@ -1,34 +1,38 @@
 #pragma once
 #include <abyss/commons/Fwd.hpp>
 #include <abyss/modules/GameObject/IComponent.hpp>
-#include <abyss/modules/DrawManager/DrawLayer.hpp>
+#include <abyss/components/Effect/base/IDraw.hpp>
 #include <abyss/utils/Coro/Task/Task.hpp>
 
 namespace abyss::Effect
 {
-    class Drawer final: public IComponent
+    class Drawer final:
+        public IComponent,
+        public IDraw
     {
     public:
         Drawer(EffectObj* pObj);
 
-        void onStart() override;
-        void draw() const;
-
-        DrawLayer getLayer() const
-        {
-            return m_layer;
-        }
         void setLayer(DrawLayer layer)
         {
             m_layer = layer;
         }
-        double getOrder() const
-        {
-            return m_order;
-        }
+
         void setOrder(double order)
         {
             m_order = order;
+        }
+    public:
+        void onStart() override;
+        void draw() const;
+
+        DrawLayer getLayer() const override
+        {
+            return m_layer;
+        }
+        double getOrder() const override
+        {
+            return m_order;
         }
     private:
         EffectObj* m_pObj;
