a02c08cc0177a3a80f06b1e3cdc9a1f2df2ec521
tyanmahou <tyanmahou@gmail.com>
2022-05-15 23:27:25 +0900

    update Boss Dead

--- Re-Abyss/app/components/Actor/Enemy/BossUtil.cpp
+++ Re-Abyss/app/components/Actor/Enemy/BossUtil.cpp
@@ -7,6 +7,7 @@
 
 #include <abyss/components/Actor/Common/AudioSource.hpp>
 #include <abyss/components/Actor/Common/Body.hpp>
+#include <abyss/components/Actor/Common/BossFadeMask.hpp>
 #include <abyss/components/Actor/utils/BehaviorUtil.hpp>
 #include <abyss/components/Effect/Actor/Common/EnemyDead/Builder.hpp>
 #include <abyss/components/Effect/Common/BossFadeMask.hpp>
@@ -15,6 +16,42 @@
 
 #include <Siv3D.hpp>
 
+namespace
+{
+	using namespace abyss;
+
+	class MultiTask
+	{
+	public:
+		MultiTask() = default;
+
+		MultiTask& add(const std::function<Coro::Task<>()>& task)
+		{
+			m_tasks.push_back(task);
+			return *this;
+		}
+
+		Coro::Task<> operator()()
+		{
+			s3d::Array<Coro::Task<>> tasks;
+			for (auto&& task : m_tasks) {
+				tasks.push_back(task());
+			}
+			while (true) {
+				for (auto&& task : tasks) {
+					task.moveNext();
+				}
+				if (tasks.all([](const Coro::Task<>& task) {return task.isDone(); })) {
+					break;
+				}
+				co_yield{};
+			}
+			co_return;
+		}
+	private:
+		s3d::Array<std::function<Coro::Task<>()>> m_tasks;
+	};
+}
 namespace abyss::Actor::Enemy
 {
 	Coro::Task<> BossUtil::DeadDemo(ActorObj* pActor)
@@ -22,32 +59,48 @@ namespace abyss::Actor::Enemy
 		co_await BehaviorUtil::WaitForSeconds(pActor, 1.0);
 		// 爆発
 		{
-			// ボスフェード開始
-			auto* bossFade = pActor->getModule<SpecialEffects>()->bossFade();
-			bossFade->start();
+			MultiTask multiTask{};
+			
+			multiTask.add([pActor]()->Coro::Task<>{
+				// ボスフェード開始
+				auto* bossFade = pActor->getModule<SpecialEffects>()->bossFade();
+				bossFade->start();
 
-			auto region = pActor->find<Body>()->region();
+				auto region = pActor->find<Body>()->region();
 
-			const auto flushCounts = Array{4, 5, 11, 12, 18, 19, 25, 26};
-			size_t flushCountIndex = 0;
-			const size_t flushCountsSize = flushCounts.size();
+				const auto flushCounts = Array{ 4, 5, 11, 12, 18, 19, 25, 26 };
+				size_t flushCountIndex = 0;
+				const size_t flushCountsSize = flushCounts.size();
 
-			for (int32 count : step(45)) {
-				if (flushCountIndex < flushCountsSize && count == flushCounts[flushCountIndex]) {
-					++flushCountIndex;
-					// 画面フラッシュ
-					pActor->getModule<SpecialEffects>()->flush()->start(0.1);
+				for (int32 count : step(45)) {
+					if (flushCountIndex < flushCountsSize && count == flushCounts[flushCountIndex]) {
+						++flushCountIndex;
+						// 画面フラッシュ
+						pActor->getModule<SpecialEffects>()->flush()->start(0.1);
+					}
+					auto effectPos = s3d::RandomVec2(region);
+					RequestDeadEffect(pActor, effectPos);
+
+					pActor->find<AudioSource>()->playAt(U"Damage", effectPos);
+
+					co_await BehaviorUtil::WaitForSeconds(pActor, 0.2);
 				}
-				auto effectPos = s3d::RandomVec2(region);
-				RequestDeadEffect(pActor, effectPos);
-
-				pActor->find<AudioSource>()->playAt(U"Damage", effectPos);
-
-				co_await BehaviorUtil::WaitForSeconds(pActor, 0.2);
-			}
-			co_await Coro::WaitUntil([&] {
-				return bossFade->isFadeOutEnd();
+				co_await Coro::WaitUntil([&] {
+					return bossFade->isFadeOutEnd();
+				});
 			});
+			multiTask.add([pActor]()->Coro::Task<> {
+				auto bossFadeMask = pActor->find<BossFadeMask>();
+
+				TimeLite::Timer timer{ 8.0 };
+				while (!timer.isEnd()) {
+					timer.update(pActor->deltaTime());
+					bossFadeMask->setRate(timer.rate());
+					co_yield{};
+				}
+			});
+
+			co_await multiTask();
 		}
 		pActor->destroy();
 	}
