087559adee7834a18cae00d962d02eda147021f1
tyanmahou <tyanmahou@gmail.com>
2022-06-09 18:59:58 +0900

    add layout window

---
+++ Re-Abyss/app/utils/Layout/Window/Window.cpp
@@ -0,0 +1,276 @@
+#include <abyss/utils/Layout/Window/Window.hpp>
+#include <Siv3D.hpp>
+
+namespace
+{
+    enum class GrabState
+    {
+        None,
+        Top, Bottom, Left, Right,
+        Tl, Tr, Bl, Br,
+        Move,
+        Scroll,
+    };
+
+    s3d::Optional<CursorStyle> GetCursorStyle(GrabState grab)
+    {
+        switch (grab) {
+        case GrabState::Tl:
+            return CursorStyle::ResizeNWSE;
+        case GrabState::Tr:
+            return CursorStyle::ResizeNESW;
+        case GrabState::Bl:
+            return CursorStyle::ResizeNESW;
+        case GrabState::Br:
+            return CursorStyle::ResizeNWSE;
+        case GrabState::Top:
+            return CursorStyle::ResizeUpDown;
+        case GrabState::Bottom:
+            return CursorStyle::ResizeUpDown;
+        case GrabState::Left:
+            return CursorStyle::ResizeLeftRight;
+        case GrabState::Right:
+            s3d::Cursor::RequestStyle(CursorStyle::ResizeLeftRight);
+            return CursorStyle::ResizeLeftRight;
+        case GrabState::Move:
+            return CursorStyle::Default;
+        default:
+            break;
+        }
+        return s3d::none;
+    }
+
+    struct WindowResizer
+    {
+    public:
+        void update(Vec2& pos, Vec2& size)
+        {
+            constexpr double margin = 5.0;
+            constexpr Vec2 minSize{ margin, margin };
+            if (!m_isGrab) {
+                if (auto grabState = grabWatch(pos, size, margin)) {
+                    m_isGrab = true;
+                    m_grabState = *grabState;
+                    m_grabPos = pos;
+                    m_grabSize = size;
+                    m_grabCursorPos = s3d::Cursor::PosF();
+                }
+            }
+            else if (!MouseL.pressed()) {
+                m_isGrab = false;
+            }
+            if (m_isGrab) {
+                this->grabUpdate(pos, size, minSize);
+            }
+        }
+    private:
+        s3d::Optional<GrabState> grabWatch(const Vec2& pos, const Vec2& size, double margin)
+        {
+            const auto rect = RectF{ pos, size };
+
+            const RectF tl{ rect.x - margin, rect.y - margin, margin, margin };
+            const RectF tr{ rect.x + rect.w, rect.y - margin, margin, margin };
+            const RectF bl{ rect.x - margin, rect.y + rect.h, margin, margin };
+            const RectF br{ rect.x + rect.w, rect.y + rect.h, margin, margin };
+
+            const RectF    top{ rect.x, rect.y - margin , rect.w, margin };
+            const RectF bottom{ rect.x, rect.y + rect.h , rect.w, margin };
+            const RectF   left{ rect.x - margin, rect.y, margin, rect.h };
+            const RectF  right{ rect.x + rect.w, rect.y, margin, rect.h };
+
+            if (tl.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeNWSE);
+            }
+            else if (tr.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeNESW);
+            }
+            else if (bl.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeNESW);
+            }
+            else if (br.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeNWSE);
+            }
+            else if (top.mouseOver() || bottom.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeUpDown);
+            }
+            else if (left.mouseOver() || right.mouseOver()) {
+                s3d::Cursor::RequestStyle(CursorStyle::ResizeLeftRight);
+            }
+
+            if (tl.leftClicked()) {
+                return GrabState::Tl;
+            }
+            else if (tr.leftClicked()) {
+                return GrabState::Tr;
+            }
+            else if (bl.leftClicked()) {
+                return GrabState::Bl;
+            }
+            else if (br.leftClicked()) {
+                return GrabState::Br;
+            }
+            else if (top.leftClicked()) {
+                return GrabState::Top;
+            }
+            else if (bottom.leftClicked()) {
+                return GrabState::Bottom;
+            }
+            else if (left.leftClicked()) {
+                return GrabState::Left;
+            }
+            else if (right.leftClicked()) {
+                return GrabState::Right;
+            }
+            else if (rect.leftClicked()) {
+                return GrabState::Move;
+            }
+            return s3d::none;
+        }
+        void grabUpdate(Vec2& pos, Vec2& size, const Vec2& minSize)
+        {
+            auto delta = Cursor::PosF() - m_grabCursorPos;
+
+            ::GetCursorStyle(m_grabState).then([](CursorStyle stlye) {
+                s3d::Cursor::RequestStyle(stlye);
+            });
+            if (m_grabState == GrabState::Move) {
+                // 移動
+                pos = m_grabPos + delta;
+            }
+            else {
+                // リサイズ
+                this->grabResize(pos, size, delta, minSize);
+            }
+        }
+        void grabResize(Vec2& pos, Vec2& size, Vec2 delta, const Vec2& minSize)
+        {
+            if (m_grabState == GrabState::Top || m_grabState == GrabState::Tl || m_grabState == GrabState::Tr) {
+                // 上
+                if (m_grabSize.y - delta.y < minSize.y) {
+                    delta.y = -(minSize.y - m_grabSize.y);
+                }
+                pos.y = m_grabPos.y + delta.y;
+                size.y = m_grabSize.y - delta.y;
+            }
+            else if (m_grabState == GrabState::Bottom || m_grabState == GrabState::Bl || m_grabState == GrabState::Br) {
+                // 下
+                if (m_grabSize.y + delta.y < minSize.y) {
+                    delta.y = minSize.y - m_grabSize.y;
+                }
+                size.y = m_grabSize.y + delta.y;
+            }
+
+            if (m_grabState == GrabState::Left || m_grabState == GrabState::Tl || m_grabState == GrabState::Bl) {
+                // 左
+                if (m_grabSize.x - delta.x < minSize.x) {
+                    delta.x = -(minSize.x - m_grabSize.x);
+                }
+                pos.x = m_grabPos.x + delta.x;
+                size.x = m_grabSize.x - delta.x;
+            }
+            else if (m_grabState == GrabState::Right || m_grabState == GrabState::Tr || m_grabState == GrabState::Br) {
+                // 右
+                if (m_grabSize.x + delta.x < minSize.x) {
+                    delta.x = minSize.x - m_grabSize.x;
+                }
+                size.x = m_grabSize.x + delta.x;
+            }
+        }
+    private:
+        bool m_isGrab = false;
+        s3d::Vec2 m_grabCursorPos{};
+        s3d::Vec2 m_grabPos{};
+        s3d::Vec2 m_grabSize{};
+        GrabState m_grabState{};
+    };
+}
+namespace abyss::Layout
+{
+    class Window::Handle
+    {
+    public:
+        Handle(const s3d::Vec2& pos, const s3d::Vec2& size) :
+            m_pos(pos),
+            m_size(size)
+        {}
+        void setBackground(const s3d::Optional<s3d::ColorF>& color)
+        {
+            m_backGroundColor = color;
+        }
+        void setFrameColor(const s3d::Optional<s3d::ColorF>& color)
+        {
+            m_frameColor = color;
+        }
+        void update()
+        {
+            m_resizer.update(m_pos, m_size);
+        }
+        [[nodiscard]] s3d::Rect region() const
+        {
+            return this->regionF().asRect();
+        }
+        [[nodiscard]] s3d::RectF regionF() const
+        {
+            return { m_pos, m_size };
+        }
+        s3d::ScopedViewport2D startViewport() const
+        {
+            return s3d::ScopedViewport2D{ this->region() };
+        }
+        s3d::Transformer2D transformer() const
+        {
+            return { Mat3x2::Identity(), Mat3x2::Translate(m_pos) };
+        }
+        s3d::RectF draw(std::function<void(const s3d::RectF&)> scene)
+        {
+            this->update();
+
+            auto region = this->regionF();
+            if (m_backGroundColor) {
+                region.draw(*m_backGroundColor);
+            }
+            {
+                auto t2d = this->transformer();
+                auto viewport = this->startViewport();
+                scene(region);
+            }
+            if (m_frameColor) {
+                region.drawFrame(2, *m_frameColor);
+            }
+            return region;
+        }
+    private:
+        s3d::Vec2 m_pos{};
+        s3d::Vec2 m_size{};
+        s3d::Optional<ColorF> m_backGroundColor{};
+        s3d::Optional<ColorF> m_frameColor{};
+        WindowResizer m_resizer;
+    };
+
+    Window::Window() :
+        Window({ 100, 100 })
+    {}
+    Window::Window(double w, double h) :
+        Window(Vec2{ w, h })
+    {}
+    Window::Window(const s3d::Vec2& size) :
+        Window({ 0, 0 }, size)
+    {}
+    Window::Window(const s3d::Vec2& pos, const s3d::Vec2& size) :
+        m_pHandle(std::make_shared<Handle>(pos, size))
+    {}
+    Window& Window::setBackGround(const s3d::Optional<s3d::ColorF>& color)
+    {
+        m_pHandle->setBackground(color);
+        return *this;
+    }
+    Window& Window::setFrameColor(const s3d::Optional<s3d::ColorF>& color)
+    {
+        m_pHandle->setFrameColor(color);
+        return *this;
+    }
+    s3d::RectF Window::draw(std::function<void(const s3d::RectF&)> scene) const
+    {
+        return m_pHandle->draw(std::move(scene));
+    }
+}
---
+++ Re-Abyss/app/utils/Layout/Window/Window.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <memory>
+#include <Siv3D/Rect.hpp>
+#include <Siv3D/RectF.hpp>
+#include <Siv3D/Vector2D.hpp>
+
+namespace abyss::Layout
+{
+    class Window
+    {
+        class Handle;
+    public:
+        Window();
+        Window(double w, double h);
+        Window(const s3d::Vec2& size);
+        Window(const s3d::Vec2& pos, const s3d::Vec2& size);
+
+        Window& setBackGround(const s3d::Optional<s3d::ColorF>& color);
+        Window& setFrameColor(const s3d::Optional<s3d::ColorF>& color);
+
+        s3d::RectF draw(std::function<void(const s3d::RectF&)> scene) const;
+    private:
+        std::shared_ptr<Handle> m_pHandle;
+    };
+}
