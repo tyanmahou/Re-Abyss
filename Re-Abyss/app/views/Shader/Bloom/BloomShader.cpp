39127e4bdb0eb0dee4def7261904d59e5a722ecc
tyanmahou <tyanmahou@gmail.com>
2022-07-27 02:14:13 +0900

    update bloom

--- Re-Abyss/app/views/Shader/Bloom/BloomShader.cpp
+++ Re-Abyss/app/views/Shader/Bloom/BloomShader.cpp
@@ -19,10 +19,13 @@ namespace abyss
     public:
         Impl():
             m_ps(Resource::Assets::Norelease()->load(U"bloom_brightness.hlsl")),
+            m_scene(Size),
             m_blurA(Size),
             m_blurB(Size),
-            m_blur8A(Size / 8),
-            m_blur8B(Size / 8)
+            m_blur4A(Size / 4),
+            m_blur4B(Size / 4),
+            m_blurC(Size),
+            m_blurD(Size)
         {}
         void setLightColor(const s3d::ColorF& color)
         {
@@ -30,20 +33,15 @@ namespace abyss
         }
         void apply(const s3d::Texture& tex) 
         {
+            // 元シーンのぼかしを生成
             {
-                s3d::ScopedRenderTarget2D target(m_blurA.clear(ColorF(0, 1)));
-                s3d::ScopedCustomShader2D scopedShader(m_ps);
-                s3d::Graphics2D::SetConstantBuffer(s3d::ShaderStage::Pixel, 1, m_cb);
-                tex.draw();
+                s3d::Shader::GaussianBlur(tex, m_blurB, m_blurA);
+                Shader::Downsample(m_blurA, m_blur4A);
+                s3d::Shader::GaussianBlur(m_blur4A, m_blur4B, m_blur4A);
             }
+            // ぼかしとmax合成
             {
-                s3d::Shader::GaussianBlur(m_blurA, m_blurB, m_blurA);
-                Shader::Downsample(m_blurA, m_blur8A);
-                s3d::Shader::GaussianBlur(m_blur8A, m_blur8B, m_blur8A);
-            }
-            tex.draw();
-            {
-                s3d::ScopedRenderTarget2D target(m_blurB.clear(ColorF(0, 1)));
+                s3d::ScopedRenderTarget2D target(m_scene.clear(ColorF(0, 1)));
                 BlendState b{
                     true,
                     Blend::One,
@@ -51,19 +49,35 @@ namespace abyss
                     BlendOp::Max
                 };
                 s3d::ScopedRenderStates2D blend(b, SamplerState::BorderLinear);
-                m_blurA.draw();
-                m_blur8A.resized(Size).draw();
+                tex.draw();
+                m_blur4A.resized(Size).draw(ColorF(0.8));
+            }
+            // 描画
+            m_scene.draw();
+
+            // 輝度抽出
+            {
+                s3d::ScopedRenderTarget2D target(m_blurC.clear(ColorF(0, 1)));
+                s3d::ScopedCustomShader2D scopedShader(m_ps);
+                s3d::Graphics2D::SetConstantBuffer(s3d::ShaderStage::Pixel, 1, m_cb);
+                m_scene.draw();
             }
             {
+                s3d::Shader::GaussianBlur(m_blurC, m_blurD, m_blurC);
+            }
+            // 加算描画
+            {
                 s3d::ScopedRenderStates2D blend(BlendState::Additive);
-                m_blurB.draw();
+                m_blurC.draw();
             }
         }
     private:
         PixelShader m_ps;
         ConstantBuffer<ShaderParam> m_cb;
+        RenderTexture m_scene;
         RenderTexture m_blurA, m_blurB;
-        RenderTexture m_blur8A, m_blur8B;
+        RenderTexture m_blur4A, m_blur4B;
+        RenderTexture m_blurC, m_blurD;
     };
     BloomShader::BloomShader() :
         m_pImpl(std::make_shared<Impl>())
