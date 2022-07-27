911d4edce1d105fc987579abf2ffc1f498f4cdc6
tyanmahou <tyanmahou@gmail.com>
2022-07-28 02:14:58 +0900

    update moisture

--- Re-Abyss/build/resources/shaders/moisture.hlsl
+++ Re-Abyss/build/resources/shaders/moisture.hlsl
@@ -23,24 +23,24 @@ float4 PS(PSInput input) : SV_TARGET
 {
 	const float2 uv = input.uv;
 	float4 tex0 = g_texture0.Sample(g_sampler0, uv);
-	float bright0 = dot(tex0.rgb, float3(0.2126, 0.7152, 0.0722));
 
 	float4 tex1 = g_texture1.Sample(g_sampler0, uv);
 	float4 tex2 = g_texture2.Sample(g_sampler0, uv);
 	float4 tex3 = g_texture3.Sample(g_sampler0, uv);
-
 	float4 blur = tex1 * 0.1 + tex2 * 0.3 + tex3 * 0.6;
-	blur.rgb = (2 * blur.rgb + (ceil(blur.rgb * 8.0) / 8.0)) / 3.0;
-	float brightB = dot(blur.rgb, float3(0.2126, 0.7152, 0.0722));
+
+	// ポスタリゼーション
+	float3 post = blur.rgb;
+	post = (ceil(post * 8.0) / 8.0);
+	// ぼかしと合成
+	post = (5.0 * blur.rgb +  11.0 * post) / 16;
+
+	// コントラスト調整
+	post = 1 / (1 + exp(-8 * (post - 0.5)));
 
 	float4 ret = tex0;
-	ret.rgb = max(
-		tex0.rgb,
-		lerp(
-			tex0.rgb,
-			blur.rgb,
-			1 - (1 - bright0) * (1 - brightB)
-		)
-	);
+
+	// srcと合成
+	ret.rgb = (2 * tex0.rgb + post) / 3.0;
 	return ret;
 }
