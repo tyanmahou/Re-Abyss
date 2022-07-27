129aab1e374cfbfb8f508cf965657ff2b61bc255
tyanmahou <tyanmahou@gmail.com>
2022-07-27 23:44:48 +0900

    update moisture

--- Re-Abyss/build/resources/shaders/moisture.hlsl
+++ Re-Abyss/build/resources/shaders/moisture.hlsl
@@ -30,7 +30,7 @@ float4 PS(PSInput input) : SV_TARGET
 	float4 tex3 = g_texture3.Sample(g_sampler0, uv);
 
 	float4 blur = tex1 * 0.1 + tex2 * 0.3 + tex3 * 0.6;
-	blur.rgb = floor(blur.rgb * 8.0) / 8.0;
+	blur.rgb = (2 * blur.rgb + (ceil(blur.rgb * 8.0) / 8.0)) / 3.0;
 	float brightB = dot(blur.rgb, float3(0.2126, 0.7152, 0.0722));
 
 	float4 ret = tex0;
