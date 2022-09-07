a0fa4adb3a6c20b3f1ff829f5487a0258a255f83
tyanmahou <tyanmahou@gmail.com>
2022-09-07 23:42:53 +0900

    update Terrain Tag

--- Re-Abyss/app/components/Actor/Gimmick/ShutterWall/Builder.cpp
+++ Re-Abyss/app/components/Actor/Gimmick/ShutterWall/Builder.cpp
@@ -22,6 +22,7 @@ namespace abyss::Actor::Gimmick::ShutterWall
             pActor->attach<TerrainProxy>(pActor)
                 ->setColDirection(ColDirection::All)
                 .setRegion(ShutterUtil::RegionFromCenter(entity.pos))
+                .setTag(Physics::Tag::Floor{})
                 ;
         }
         // 座標
