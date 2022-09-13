600c2559d121c488deb2f5cc4ce055951f879f78
tyanmahou <tyanmahou@gmail.com>
2022-09-14 02:55:53 +0900

    fix color.hlsl

--- Re-Abyss/build/resources/shaders/color.hlsl
+++ Re-Abyss/build/resources/shaders/color.hlsl
@@ -30,13 +30,12 @@ float3 hue2rgb(in float h)
     float b = 2 - abs(h * 6 - 4);
     return saturate(float3(r, g, b));
 }
-
-float3 hsv2rgb(in float3 hsv)
+float3 hsl2rgb(in float3 hsl)
 {
-    float3 rgb = hue2rgb(hsv.x);
-    return ((rgb - 1) * hsv.y + 1) * hsv.z;
+    float3 rgb = hue2rgb(hsl.x);
+    float c = (1 - abs(2 * hsl.z - 1)) * hsl.y;
+    return (rgb - 0.5) * c + hsl.z;
 }
-
 static const float epsilon = 1e-10;
 float3 rgb2hcv(in float3 rgb)
 {
@@ -46,26 +45,28 @@ float3 rgb2hcv(in float3 rgb)
     float h = abs((q.w - q.y) / (6 * c + epsilon) + q.z);
     return float3(h, c, q.x);
 }
-float3 rgb2hsv(in float3 rgb)
+float3 rgb2hsl(in float3 rgb)
 {
     float3 hcv = rgb2hcv(rgb);
-    float s = hcv.y / (hcv.z + epsilon);
-    return float3(hcv.x, s, hcv.z);
+    float l = hcv.z - hcv.y * 0.5;
+    float s = hcv.y / (1 - abs(l * 2 - 1) + epsilon);
+    return float3(hcv.x, s, l);
 }
 
 // カラー合成
 float4 color(in float4 dest, in float4 src)
 {
-    float3 hsvDest = rgb2hsv(dest.rgb);
-    float3 hsvSrc = rgb2hsv(src.rgb * src.a);
+    float3 hslDest = rgb2hsl(dest.rgb);
+    float3 hslSrc = rgb2hsl(src.rgb);
 
     // 色相、彩度を上書き
-    float3 hsvRet = hsvDest;
-    hsvRet.b = hsvDest.b;
-    
+    float3 hslRet = hslDest;
+    hslRet.r = hslSrc.r;
+    hslRet.g = hslSrc.g;
+
     float4 ret;
-    ret.rgb = hsv2rgb(hsvRet);
-    ret.a = dest.a;
+    ret.rgb = hsl2rgb(hslRet);
+    ret.a = dest.a * src.a;
     return ret;
 }
 float4 PS(PSInput input) : SV_TARGET
