b1ccae0f2c87bb90b98bba45d33361acfee04778
tyanmahou <tyanmahou@gmail.com>
2022-10-07 23:51:51 +0900

    add home stream

---
+++ Re-Abyss/app/components/Event/Home/MainStream.cpp
@@ -0,0 +1,23 @@
+#include <abyss/components/Event/Home/MainStream.hpp>
+
+#include <abyss/modules/Event/base/EventObj.hpp>
+
+namespace abyss::Event::Home
+{
+    MainStream::MainStream(EventObj* pEvent):
+        m_pEvent(pEvent)
+    {}
+    void MainStream::setup(Executer executer)
+    {
+    }
+    void MainStream::onStart()
+    {
+    }
+    void MainStream::onEnd()
+    {
+    }
+    Coro::Task<> MainStream::onExecute()
+    {
+        co_return;
+    }
+}
