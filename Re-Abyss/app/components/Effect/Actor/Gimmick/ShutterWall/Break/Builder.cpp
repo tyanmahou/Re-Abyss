259ef1d0d049774648d13bccf08854daeeb4052d
tyanmahou <tyanmahou@gmail.com>
2022-12-15 17:54:51 +0900

    add break effect empty

---
+++ Re-Abyss/app/components/Effect/Actor/Gimmick/ShutterWall/Break/Builder.cpp
@@ -0,0 +1,11 @@
+#include <abyss/components/Effect/Actor/Gimmick/ShutterWall/Break/Builder.hpp>
+
+#include <abyss/modules/Effect/base/EffectObj.hpp>
+
+namespace abyss::Effect::Actor::Gimmick::ShutterWall::Break
+{
+    void Builder::Build(EffectObj* pObj, const s3d::Vec2& pos)
+    {
+        // TODO 実装
+    }
+}
