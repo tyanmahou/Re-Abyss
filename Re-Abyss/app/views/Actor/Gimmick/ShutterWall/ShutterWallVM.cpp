d13152ff2c282ff9e422c0e78b3b10fe21746966
tyanmahou <tyanmahou@gmail.com>
2022-12-16 23:28:22 +0900

    update shutter

--- Re-Abyss/app/views/Actor/Gimmick/ShutterWall/ShutterWallVM.cpp
+++ Re-Abyss/app/views/Actor/Gimmick/ShutterWall/ShutterWallVM.cpp
@@ -1,18 +1,18 @@
 #include <abyss/views/Actor/Gimmick/ShutterWall/ShutterWallVM.hpp>
+#include <abyss/commons/Resource/Assets/Assets.hpp>
 #include <abyss/params/Actor/Gimmick/ShutterWall/Param.hpp>
 
 namespace abyss::Actor::Gimmick::ShutterWall
 {
-    ShutterWallVM::ShutterWallVM()
+    ShutterWallVM::ShutterWallVM():
+        m_texture(Resource::Assets::Main()->load(U"Actor/Gimmick/ShutterWall/ShutterWall.json"))
     {
     }
     void ShutterWallVM::draw() const
     {
         const Vec2& baseSize = Param::BaseSize;
-        RectF(
-            m_pos - baseSize / 2,
-            baseSize.x,
-            baseSize.y * m_shutterRate
-        ).draw(m_colorMul);
+        m_texture(U"Wall")(0, 0, baseSize.x, baseSize.y * m_shutterRate)
+            .draw(m_pos - baseSize / 2, m_colorMul);
+
     }
 }
