12f7e2a85e2cacb75cef0a5050f651f0e430b900
tyanmahou <tyanmahou@gmail.com>
2022-12-19 22:22:29 +0900

    update id

--- Re-Abyss/app/components/Actor/Gimmick/ShutterWall/DamageCallback.cpp
+++ Re-Abyss/app/components/Actor/Gimmick/ShutterWall/DamageCallback.cpp
@@ -4,15 +4,19 @@
 #include <abyss/modules/Camera/Camera.hpp>
 #include <abyss/modules/Stage/Stage.hpp>
 #include <abyss/modules/Temporary/Temporary.hpp>
+#include <abyss/components/Actor/Common/ActorId.hpp>
 #include <abyss/components/Actor/Common/AudioSource.hpp>
 
 namespace
 {
     using namespace abyss;
     using namespace abyss::Actor;
-    TempKey ShutterWallBreakKey(ActorObj* pActor, s3d::uint32 id)
+    TempKey ShutterWallBreakKey(ActorObj* pActor)
     {
-        return TempKey::ShutterWallBreak(pActor->getModule<Stage>()->mapName(), id);
+        return TempKey::ShutterWallBreak(
+            pActor->getModule<Stage>()->mapName(),
+            pActor->find<ActorId>()->id()
+        );
     }
 }
 namespace abyss::Actor::Gimmick::ShutterWall
@@ -24,7 +28,7 @@ namespace abyss::Actor::Gimmick::ShutterWall
     void DamageCallback::onStart()
     {
         // 取得済みなら破棄
-        if (m_pActor->getModule<Temporary>()->onFlag(::ShutterWallBreakKey(m_pActor, 0))) {
+        if (m_pActor->getModule<Temporary>()->onFlag(::ShutterWallBreakKey(m_pActor))) {
             m_pActor->destroy();
             return;
         }
@@ -44,6 +48,6 @@ namespace abyss::Actor::Gimmick::ShutterWall
         m_pActor->getModule<Camera>()->startQuake(5.0, 0.3);
 
         // フラグ保存
-        m_pActor->getModule<Temporary>()->saveFlagRestart(::ShutterWallBreakKey(m_pActor, 0));
+        m_pActor->getModule<Temporary>()->saveFlagRestart(::ShutterWallBreakKey(m_pActor));
     }
 }
