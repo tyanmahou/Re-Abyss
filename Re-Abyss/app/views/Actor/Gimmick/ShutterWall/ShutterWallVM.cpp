a8bfa94241ee401a45537079ab10fac4e38a64cc
tyanmahou <tyanmahou@gmail.com>
2022-12-24 07:21:54 +0900

    update Shutter VM

--- Re-Abyss/app/views/Actor/Gimmick/ShutterWall/ShutterWallVM.cpp
+++ Re-Abyss/app/views/Actor/Gimmick/ShutterWall/ShutterWallVM.cpp
@@ -1,6 +1,7 @@
 #include <abyss/views/Actor/Gimmick/ShutterWall/ShutterWallVM.hpp>
 #include <abyss/commons/Resource/Assets/Assets.hpp>
 #include <abyss/params/Actor/Gimmick/ShutterWall/Param.hpp>
+#include <Siv3D.hpp>
 
 namespace abyss::Actor::Gimmick::ShutterWall
 {
@@ -11,8 +12,15 @@ namespace abyss::Actor::Gimmick::ShutterWall
     void ShutterWallVM::draw() const
     {
         const Vec2& baseSize = Param::BaseSize;
-        m_texture(U"Wall")(0, 0, baseSize.x, baseSize.y * m_shutterRate)
-            .draw(m_pos - baseSize / 2, m_colorMul);
+        const Vec2 tl = m_pos - baseSize / 2;
+        const double height = baseSize.y * m_shutterRate;
+        m_texture(U"wall")(0, 0, baseSize.x, height)
+            .draw(tl, m_colorMul);
 
+        auto&& headTex = m_texture(U"head");
+        // 下
+        headTex.flipped().draw(tl + Vec2{ -10, Max(-2.0, height - headTex.size().y) });
+        // 上
+        headTex(0, 0, headTex.size().x, Clamp<double>(height, 7.0, headTex.size().y)).draw(tl + Vec2{-10, 0});
     }
 }
