01aac97395e23886b6521e818cee2468ee95bd62
tyanmahou <tyanmahou@gmail.com>
2022-12-26 01:24:37 +0900

    attach comp

--- Re-Abyss/app/components/Effect/Actor/Gimmick/ShutterWall/Break/Builder.cpp
+++ Re-Abyss/app/components/Effect/Actor/Gimmick/ShutterWall/Break/Builder.cpp
@@ -1,11 +1,12 @@
 #include <abyss/components/Effect/Actor/Gimmick/ShutterWall/Break/Builder.hpp>
 
 #include <abyss/modules/Effect/base/EffectObj.hpp>
+#include <abyss/components/Effect/Actor/Gimmick/ShutterWall/Break/Main.hpp>
 
 namespace abyss::Effect::Actor::Gimmick::ShutterWall::Break
 {
     void Builder::Build(EffectObj* pObj, const s3d::Vec2& pos)
     {
-        // TODO 実装
+        pObj->attach<Main>(pObj, pos);
     }
 }
