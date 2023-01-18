2ac89e938bc65b64ab1b160396762b5d1ddb08be
tyanmahou <tyanmahou@gmail.com>
2023-01-19 00:29:30 +0900

    fix extends

---
+++ Re-Abyss/app/utils/Network/GitHub/Issue/Issue.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <abyss/utils/JSON/JSONUtil.hpp>
+
+namespace abyss::Network::GitHub::Issue
+{
+    /// <summary>
+    /// GitHub Issue
+    /// </summary>
+    struct Issue
+    {
+        [[JSON_BIND(id)]]
+        s3d::int32 id;
+
+        [[JSON_BIND(number)]]
+        s3d::int32 number;
+
+        [[JSON_BIND(title)]]
+        s3d::String title;
+    };
+
+    using IssueList = s3d::Array<Issue>;
+}
