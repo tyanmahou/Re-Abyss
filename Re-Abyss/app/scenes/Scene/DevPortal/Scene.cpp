37ee6ad49b77c10bcca5e90b82a7a0686a6ad105
tyanmahou <tyanmahou@gmail.com>
2023-01-22 23:43:51 +0900

    update TaskBoard

--- Re-Abyss/app/scenes/Scene/DevPortal/Scene.cpp
+++ Re-Abyss/app/scenes/Scene/DevPortal/Scene.cpp
@@ -3,23 +3,63 @@
 #include <abyss/modules/Devs/GitHub/GitHub.hpp>
 #include <Siv3D.hpp>
 
+#include <abyss/utils/Layout/Window/Window.hpp>
+#include <abyss/commons/ColorDef.hpp>
+#include <abyss/commons/Constants.hpp>
 #if ABYSS_DEVELOP
+
 namespace abyss::Scene::DevPortal
 {
+    class TaskBoard
+    {
+    public:
+        TaskBoard()
+        {
+            auto windowContext =
+                Layout::Window::WindowContext{ Vec2{Constants::AppResolutionF.x -400-10 , 10}, Vec2{400, 600} }
+                .setBackGroundColor(ColorF(0, 0.5))
+                .setFrameColor(s3d::Palette::White)
+                .setScrollBarColor(ColorF(0, 0.5))
+                .setScrollGripColor(ColorF(0.9))
+                .setCanResize(true)
+                .setIsResizeClampSceneSize(false)
+                ;
+            m_window = std::make_unique<Layout::Window::Window>(windowContext);
+
+        }
+
+        void draw() const
+        {
+            m_window->draw([&](const RectF& sceneScreen) {
+                const Vec2 columnSize{ sceneScreen.w, m_font.height(16)};
+                size_t index = 0;
+                // In Progressタスク
+                for (auto&& issue : m_gitHub.getIssues(U"In Progress")) {
+                    RectF column(0, columnSize.y * index, columnSize);
+                    column.draw(ColorF(s3d::Palette::Greenyellow, 0.9));
+                    m_font(issue.title).draw(column, Palette::Black);
+                    ++index;
+                }
+                // Todoタスク
+                for (auto&& issue : m_gitHub.getIssues(U"Todo")) {
+                    RectF column(0, columnSize.y * index, columnSize);
+                    column.draw(ColorF(s3d::Palette::Aquamarine, 0.9));
+                    m_font(issue.title).draw(column, Palette::Black);
+                    ++index;
+                }
+            });
+        }
+    private:
+        Devs::GitHub::GitHub m_gitHub;
+        Font m_font{ 16, Typeface::Regular };
+        std::unique_ptr<Layout::Window::Window> m_window;
+    };
     class Scene::Impl
     {
     public:
         Impl(const InitData& init) :
             m_data(init._s)
         {
-            Debug::Log::Info(U"Todo");
-            for (auto&& issue : m_gitHub.getIssues(U"Todo")) {
-                Debug::Log::Info(issue.title);
-            }
-            Debug::Log::Info(U"In Progress");
-            for (auto&& issue : m_gitHub.getIssues(U"In Progress")) {
-                Debug::Log::Info(issue.title);
-            }
         }
 
         void update()
@@ -30,12 +70,15 @@ namespace abyss::Scene::DevPortal
         }
         void draw()
         {
+            s3d::Scene::Rect().draw(ColorDef::DefaultFadeColor);
             m_font(U"[DevPortal] Push Enter Start").draw();
+            m_task.draw();
         }
     private:
         std::shared_ptr<Data_t> m_data;
-        Devs::GitHub::GitHub m_gitHub;
         Font m_font{ 20 };
+
+        TaskBoard m_task;
     };
     Scene::Scene(const InitData& init) :
         ISceneBase(init),
