097d3b1282c3fdc7cf2818e3dcb5a3bf4f10b3e1
tyanmahou <tyanmahou@gmail.com>
2023-01-29 22:35:22 +0900

    add NameOf Test

---
+++ Re-Abyss/tests/tests/utils/NameOf/NameOfCatch.cpp
@@ -0,0 +1,36 @@
+#if ABYSS_DO_TEST
+#include <ThirdParty/Catch2/catch.hpp>
+#include <abyss/utils/NameOf/NameOf.hpp>
+#include <Siv3D.hpp>
+
+namespace
+{
+    enum TestEnum
+    {
+        A, B, C
+    };
+    enum class TestEnumClass
+    {
+        A, B, C
+    };
+    struct TestStruct
+    {};
+    struct TestClass
+    {};
+}
+
+namespace abyss::tests
+{
+    TEST_CASE("utils::NameOf. Test")
+    {
+        SECTION("test normal")
+        {
+            REQUIRE(NameOf::nameof<TestEnum>() == U"TestEnum");
+            REQUIRE(NameOf::nameof<TestEnumClass>() == U"TestEnumClass");
+            REQUIRE(NameOf::nameof<TestStruct>() == U"TestStruct");
+            REQUIRE(NameOf::nameof<TestClass>() == U"TestClass");
+        }
+    }
+}
+
+#endif
