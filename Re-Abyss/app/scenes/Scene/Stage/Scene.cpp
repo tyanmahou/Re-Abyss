a8eb2d74ee038c33e58412ea90127c31caf426f2
tyanmahou <tyanmahou@gmail.com>
2023-02-04 03:04:11 +0900

    update scene move

--- Re-Abyss/app/scenes/Scene/Stage/Scene.cpp
+++ Re-Abyss/app/scenes/Scene/Stage/Scene.cpp
@@ -56,10 +56,9 @@ namespace abyss::Scene::Stage
 				m_context.mapPath = Path::MapPath + U"Stage0/Stage0_0.tmx";
 			}
 		}
-		Coro::Generator<double> loading()
-		{
-			co_yield 0.0;
-			Resource::Assets::Main()->release();
+        Coro::Generator<double> preloadAsset()
+        {
+            Resource::Assets::Main()->release();
 #if ABYSS_DEBUG
             String path;
             if (m_context.mapPath.includes(U"tests/data/maps")) {
@@ -72,10 +71,17 @@ namespace abyss::Scene::Stage
             String path = MapName(m_context.mapPath);
             Resource::Preload::Preloader loader(path);
 #endif
-			for (auto&& p : loader.preloadProgress()) {
-				co_yield p;
-			}
-			this->init();
+            for (auto&& p : loader.preloadProgress()) {
+                co_yield p;
+            }
+        }
+		Coro::Generator<double> loading()
+		{
+			co_yield 0.0;
+            for (auto&& p : preloadAsset()) {
+                co_yield p;
+            }
+            this->init();
 			co_yield 1.0;
 		}
 #if ABYSS_NO_BUILD_RESOURCE
@@ -156,7 +162,7 @@ namespace abyss::Scene::Stage
             m_systemNext->boot(booter.get());
 			return true;
 		}
-        bool onMoveStage(const s3d::String& link, s3d::int32 startId) override
+        Coro::Generator<double> loadingOnMove(const s3d::String& link, s3d::int32 startId)
         {
             // マップ更新
 #if ABYSS_DEBUG
@@ -167,13 +173,16 @@ namespace abyss::Scene::Stage
             }
 #else
             m_context.mapPath = Path::MapPath + link + U".tmx";
-#endif            
+#endif
+            for (auto&& p : preloadAsset()) {
+                co_yield p;
+            }
             Actor::Player::PlayerDesc desc = m_system
                 ->mod<Actor::Player::PlayerManager>()
                 ->getDesc();
             desc.startId = startId;
 
-            m_systemNext = std::make_unique<System>();
+            m_system = std::make_unique<System>();
             auto injector = Factory::Stage::Injector(m_context.mapPath);
             m_stageData = injector.resolve<StageData>();
             m_stageData->setMapName(MapName(m_context.mapPath));
@@ -185,7 +194,12 @@ namespace abyss::Scene::Stage
                 .setCharaTable(m_charaTable)
                 ;
 
-            m_systemNext->boot(booter.get());
+            m_system->boot(booter.get());
+            co_yield{};
+        }
+        bool onMoveStage(const s3d::String& link, s3d::int32 startId) override
+        {
+            m_data->loader.startAsync(loadingOnMove(link, startId));
             return true;
         }
 		bool onEscape() override
