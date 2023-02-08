74f27a0aa01a63c8e7ce73b68f1b3339732cbc11
tyanmahou <tyanmahou@gmail.com>
2023-02-09 02:36:00 +0900

    update Quake

--- Re-Abyss/app/components/Actor/Enemy/CodeZero/State/AngryState.cpp
+++ Re-Abyss/app/components/Actor/Enemy/CodeZero/State/AngryState.cpp
@@ -2,18 +2,26 @@
 
 #include <abyss/components/Actor/Enemy/CodeZero/State/WaitState.hpp>
 #include <abyss/components/Actor/Common/ColorAnim/InvincibleColor.hpp>
-#include <abyss/modules/Sfx/SpecialEffects.hpp>
 #include <abyss/components/Actor/utils/BehaviorUtil.hpp>
+#include <abyss/modules/Camera/Camera.hpp>
+#include <abyss/modules/Sfx/SpecialEffects.hpp>
 
 namespace abyss::Actor::Enemy::CodeZero
 {
     void AngryState::start()
     {
+        // 無敵
         m_damageCtrl->setInvincibleState(true);
-        m_pActor->find<ColorAnim::InvincibleColor>()->startAnim(2.0);
+        m_pActor->find<ColorAnim::InvincibleColor>()->startAnim(4.0);
+
+        // 頭固定
+        m_head->setActive(false);
 
         // 画面フラッシュ
         m_pActor->getModule<SpecialEffects>()->flush()->start(0, 0.5);
+
+        // 地震
+        m_pActor->getModule<Camera>()->startQuake(10.0, 0.3);
     }
     void AngryState::end()
     {
@@ -22,7 +30,7 @@ namespace abyss::Actor::Enemy::CodeZero
 
     Coro::Fiber<> AngryState::task()
     {
-        co_await BehaviorUtil::WaitForSeconds(m_pActor, 2.0);
+        co_await BehaviorUtil::WaitForSeconds(m_pActor, 4.0);
         this->changeState<WaitState>();
     }
 
