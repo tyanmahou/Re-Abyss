9f0383acfff524ac6ed5afd0770acff2f0ab8a6c
tyanmahou <tyanmahou@gmail.com>
2023-02-23 00:41:04 +0900

    add token

---
+++ Re-Abyss/app/utils/NeptScript/Script/Lexer/Token.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <Siv3D/String.hpp>
+
+namespace abyss::Nept
+{
+    enum class TokenType
+    {
+        /// <summary>無効値</summary>
+        Invalid,
+
+        /// <summary>/</summary>
+        Slash,
+
+        /// <summary>#</summary>
+        Sharp,
+
+        /// <summary>@</summary>
+        At,
+
+        /// <summary>括弧 [</summary>
+        LBracket,
+        /// <summary>括弧 ]</summary>
+        RBracket,
+
+        /// <summary>括弧 {</summary>
+        LBrace,
+        /// <summary>括弧 }</summary>
+        RBrace,
+
+        /// <summary>識別子</summary>
+        Ident,
+
+        /// <summary>値</summary>
+        Value,
+
+        /// <summary>=</summary>
+        Colon,
+
+        /// <summary>テキスト</summary>
+        Text
+    };
+
+    /// <summary>
+    /// トークン
+    /// </summary>
+    struct Token
+    {
+        Token() = default;
+
+        Token(TokenType _type, const s3d::String& _value) :
+            type(_type),
+            value(_value)
+        {}
+        Token(TokenType _type, s3d::String&& _value) noexcept :
+            type(_type),
+            value(std::move(_value))
+        {}
+        TokenType type;
+        s3d::String value;
+    };
+}
