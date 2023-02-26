7331336a4e8e660c8409fca189b715693f60fcab
tyanmahou <tyanmahou@gmail.com>
2023-02-26 10:53:43 +0900

    add IEvaluator

---
+++ Re-Abyss/app/utils/NeptScript/Script/Evaluator/IEvaluator.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <abyss/utils/NeptScript/Script/Ast/Statement/CharaStatement.hpp>
+#include <abyss/utils/NeptScript/Script/Ast/Statement/CommandStatement.hpp>
+#include <abyss/utils/NeptScript/Script/Ast/Statement/TextStatement.hpp>
+
+namespace abyss::Nept
+{
+    class IEvaluator
+    {
+    public:
+        virtual ~IEvaluator() = default;
+
+        virtual void eval(const Ast::CharaStatement& statement) = 0;
+        virtual void eval(const Ast::CommandStatement& statement) = 0;
+        virtual void eval(const Ast::TextStatement& statement) = 0;
+
+        virtual void evalOnSectionStart(const s3d::String& section) = 0;
+        virtual void evalOnSectionEnd(const s3d::String& section) = 0;
+    };
+}
