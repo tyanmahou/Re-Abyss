8f91fc26cc653f4fe5cf5e8a6e36645aa3ebe499
tyanmahou <tyanmahou@gmail.com>
2023-03-07 01:26:51 +0900

    add comp

--- Re-Abyss/app/modules/Adv/base/AdvObj.cpp
+++ Re-Abyss/app/modules/Adv/base/AdvObj.cpp
@@ -1,14 +1,20 @@
 #include <abyss/modules/Adv/base/AdvObj.hpp>
 #include <abyss/modules/Adv/base/Engine.hpp>
+#include <abyss/modules/Adv/base/Process.hpp>
+#include <abyss/modules/Adv/base/SentenceCtrl.hpp>
 
 namespace abyss::Adv
 {
     AdvObj::AdvObj()
     {
         m_engine = this->attach<Engine>(this);
+        m_process = this->attach<Process>(this);
+        m_sentence = this->attach<SentenceCtrl>(this);
     }
     bool AdvObj::update()
     {
+        m_sentence->update();
+        m_process->update();
         return m_engine->update();
     }
 }
--- Re-Abyss/app/modules/Adv/base/AdvObj.hpp
+++ Re-Abyss/app/modules/Adv/base/AdvObj.hpp
@@ -5,6 +5,8 @@
 namespace abyss::Adv
 {
     class Engine;
+    class Process;
+    class SentenceCtrl;
 
     class AdvObj : public GameObject
     {
@@ -17,7 +19,17 @@ namespace abyss::Adv
         {
             return m_engine;
         }
+        const Ref<Process>& process() const
+        {
+            return m_process;
+        }
+        const Ref<SentenceCtrl>& sentence() const
+        {
+            return m_sentence;
+        }
     private:
         Ref<Engine> m_engine;
+        Ref<Process> m_process;
+        Ref<SentenceCtrl> m_sentence;
     };
 }
