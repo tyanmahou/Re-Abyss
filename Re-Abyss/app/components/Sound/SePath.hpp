c32deda0703f677d1b1882c55557652be9a49799
tyanmahou <tyanmahou@gmail.com>
2023-06-29 22:13:50 +0900

    fix path

--- Re-Abyss/app/components/Sound/SePath.hpp
+++ Re-Abyss/app/components/Sound/SePath.hpp
@@ -6,7 +6,7 @@ namespace abyss::Sound
     class SePath
     {
     public:
-        inline static SoundLabel TitleDecide{ U"Se/Actor/Player/Shot/Shot3" };
+        inline static SoundLabel TitleDecide{ U"Se/Actor/Player/Shot/ShotBig" };
 
         inline static SoundLabel ChargeBossHp{ U"Se/Common/ChargeBossHp" };
     };
