839ee6d77ada4ff80973cc40dd2fee332a8fb88c
tyanmahou <tyanmahou@gmail.com>
2023-07-02 20:50:01 +0900

    fix Preprocess.hpp

--- Re-Abyss/app/Preprocess.hpp
+++ Re-Abyss/app/Preprocess.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
 // デバッグ機能の使用マクロ
-#ifdef ABYSS_DEVELOP || ABYSS_STAGING
+#if defined(ABYSS_DEVELOP) || defined(ABYSS_STAGING)
 #define ABYSS_DEBUG 1
 #endif
 
 // テスト実行のマクロ
-#ifdef ABYSS_DEVELOP
+#if defined(ABYSS_DEVELOP)
 #define ABYSS_DO_TEST 1
 #endif
 
 // リソースビルドを使用しないマクロ
-#ifdef ABYSS_DEVELOP
+#if defined(ABYSS_DEVELOP)
 #define ABYSS_NO_BUILD_RESOURCE 1
 #endif
