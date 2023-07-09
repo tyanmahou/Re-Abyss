5eddcb37a296d6037b314f7c250aeb0e01f27a4c
tyanmahou <tyanmahou@gmail.com>
2023-07-09 23:40:13 +0900

    update BossArrival builder

--- Re-Abyss/app/components/UI/BossArrival/Builder.cpp
+++ Re-Abyss/app/components/UI/BossArrival/Builder.cpp
@@ -1,9 +1,11 @@
 #include <abyss/components/UI/BossArrival/Builder.hpp>
+#include <abyss/components/UI/BossArrival/Main.hpp>
+#include <abyss/modules/UI/base/UIObj.hpp>
 
 namespace abyss::UI::BossArrival
 {
     void  Builder::Build(UIObj* pUi)
     {
-
+        pUi->attach<Main>(pUi);
     }
 }
