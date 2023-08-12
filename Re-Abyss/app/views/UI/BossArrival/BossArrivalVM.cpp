28ccfa826960822ee9f67f006b684bd9a6e95133
tyanmahou <tyanmahou@gmail.com>
2023-08-12 09:02:35 +0900

    update BossArrival

--- Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.cpp
+++ Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.cpp
@@ -5,6 +5,11 @@ namespace abyss::UI::BossArrival
     BossArrivalVM::BossArrivalVM()
     {
     }
+    BossArrivalVM& BossArrivalVM::setPos(const s3d::Vec2& pos)
+    {
+        m_pos = pos;
+        return *this;
+    }
     void BossArrivalVM::draw() const
     {
     }
--- Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.hpp
+++ Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.hpp
@@ -7,6 +7,7 @@ namespace abyss::UI::BossArrival
     public:
         BossArrivalVM();
 
+        BossArrivalVM& setPos(const s3d::Vec2& pos);
         void draw() const;
     private:
         s3d::Vec2 m_pos;
