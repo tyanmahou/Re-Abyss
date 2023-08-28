38e09207f67f87df3b621b221375ee33984d9d66
tyanmahou <tyanmahou@gmail.com>
2023-08-28 22:32:14 +0900

    update BossArrivalVM

--- Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.cpp
+++ Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.cpp
@@ -1,8 +1,10 @@
 #include <abyss/views/UI/BossArrival/BossArrivalVM.hpp>
+#include <abyss/views/util/Anchor/AnchorUtil.hpp>
 
 namespace abyss::UI::BossArrival
 {
-    BossArrivalVM::BossArrivalVM()
+    BossArrivalVM::BossArrivalVM():
+        m_pos(AnchorUtil::FromCc(0 ,0))
     {
     }
     BossArrivalVM& BossArrivalVM::setPos(const s3d::Vec2& pos)
