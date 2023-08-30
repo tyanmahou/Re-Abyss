2adb6afa430f5cb8bbdfcc6a79bd65456609a91b
tyanmahou <tyanmahou@gmail.com>
2023-08-30 23:08:01 +0900

    update

--- Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.hpp
+++ Re-Abyss/app/views/UI/BossArrival/BossArrivalVM.hpp
@@ -9,9 +9,16 @@ namespace abyss::UI::BossArrival
         BossArrivalVM();
 
         BossArrivalVM& setPos(const s3d::Vec2& pos);
+
+        BossArrivalVM& setTime(double time)
+        {
+            m_time = time;
+            return *this;
+        }
         void draw() const;
     private:
         TexturePacker m_texture;
         s3d::Vec2 m_pos;
+        double m_time = 0;
     };
 }
