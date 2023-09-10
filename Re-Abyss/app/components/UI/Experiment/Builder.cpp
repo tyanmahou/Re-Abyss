bca02d1ecd612ac475eb679efc00486db701a1a7
tyanmahou <tyanmahou@gmail.com>
2023-09-10 21:05:02 +0900

    add Experiment Builder

--- Re-Abyss/app/components/UI/Experiment/Builder.cpp
+++ Re-Abyss/app/components/UI/Experiment/Builder.cpp
@@ -1,11 +1,13 @@
 #include <abyss/components/UI/Experiment/Builder.hpp>
 #if ABYSS_DEVELOP
 #include <abyss/modules/UI/base/UIObj.hpp>
+#include <abyss/components/UI/Experiment/Main.hpp>
 
 namespace abyss::UI::Experiment
 {
     void Builder::Build(UIObj* pUi)
     {
-    };
+        pUi->attach<Main>(pUi);
+    }
 }
 #endif
