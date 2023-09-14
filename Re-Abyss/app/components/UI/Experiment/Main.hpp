52528c17c957db528f3959ae70136fc632681e5b
tyanmahou <tyanmahou@gmail.com>
2023-09-14 22:21:16 +0900

    update Main

--- Re-Abyss/app/components/UI/Experiment/Main.hpp
+++ Re-Abyss/app/components/UI/Experiment/Main.hpp
@@ -3,6 +3,7 @@
 #include <abyss/modules/GameObject/IComponent.hpp>
 #include <abyss/modules/UI/base/IUpdate.hpp>
 #include <abyss/modules/UI/base/IDraw.hpp>
+#include <abyss/components/UI/Experiment/ITopic.hpp>
 
 namespace abyss::UI::Experiment
 {
@@ -14,12 +15,19 @@ namespace abyss::UI::Experiment
     public:
         Main(UIObj* pUi);
 
+        template<class T, class... Args>
+        Main& add(Args&&... arggs)
+        {
+            m_topics(std::make_shared<T>(std::forward<Args>(args),,,));
+            return *this;
+        }
     public:
         void onUpdate() override;
     public:
         void onDraw()const override;
     private:
         UIObj* m_pUi;
+        s3d::Array<std::shared_ptr<ITopic>> m_topics;
     };
 }
 namespace abyss
