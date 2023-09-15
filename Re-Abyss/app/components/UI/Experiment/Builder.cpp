9ebb37a07f4286b430407685242646e30b2c7b10
tyanmahou <tyanmahou@gmail.com>
2023-09-15 22:40:09 +0900

    update topic

--- Re-Abyss/app/components/UI/Experiment/Builder.cpp
+++ Re-Abyss/app/components/UI/Experiment/Builder.cpp
@@ -2,12 +2,15 @@
 #if ABYSS_DEVELOP
 #include <abyss/modules/UI/base/UIObj.hpp>
 #include <abyss/components/UI/Experiment/Main.hpp>
+#include <abyss/components/UI/Experiment/DualComplex/Topic.hpp>
 
 namespace abyss::UI::Experiment
 {
     void Builder::Build(UIObj* pUi)
     {
-        pUi->attach<Main>(pUi);
+        pUi->attach<Main>(pUi)
+            ->add<DualComplex::Topic>()
+            ;
     }
 }
 #endif
