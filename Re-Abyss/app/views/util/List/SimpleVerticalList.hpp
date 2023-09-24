457e1f6a12d0e0595f318cbb09a208556069fd77
tyanmahou <tyanmahou@gmail.com>
2023-09-24 22:26:26 +0900

    update SimpleVerticalList

--- Re-Abyss/app/views/util/List/SimpleVerticalList.hpp
+++ Re-Abyss/app/views/util/List/SimpleVerticalList.hpp
@@ -1,23 +1,44 @@
 #pragma once
+#include <functional>
+#include <Siv3D/RectF.hpp>
+#include <Siv3D/String.hpp>
 
 namespace abyss::UI::List
 {
     class SimpleVerticalList
     {
     public:
-        SimpleVerticalList& setScreen(const RectF& screen)
+        struct Record
+        {
+            s3d::String title;
+            std::function<void()> onClick;
+            s3d::Color backGroundColor{ 0, 0, 0, 128 };
+        };
+    public:
+        SimpleVerticalList& setScreen(const s3d::RectF& screen)
         {
             m_screen = screen;
             return *this;
         }
-        SimpleVerticalList setFontSize(double size)
+        SimpleVerticalList& setFontSize(double size)
         {
-            m_size = size;
+            m_fontSize = size;
+            return *this;
+        }
+        SimpleVerticalList& push_back(Record&& record)
+        {
+            m_records.push_back(std::move(record));
+            return *this;
+        }
+        SimpleVerticalList& clear()
+        {
+            m_records.clear();
             return *this;
         }
         void draw() const;
     public:
-        RectF m_screen;
-        double m_size;
+        s3d::RectF m_screen;
+        double m_fontSize;
+        s3d::Array<Record> m_records;
     };
 }
