f496ea0015467c58ccf607aae328d7d46c94eebd
tyanmahou <tyanmahou@gmail.com>
2023-10-26 23:50:31 +0900

    update FontAssetName

---
+++ Re-Abyss/app/commons/Resource/Font/FontAssetName.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace abyss::Resource
+{
+    class FontAssetName
+    {
+    public:
+        static inline constexpr s3d::FilePathView PixelMplus12 = U"/resources/fonts/PixelMplus12-Regular.ttf";
+        static inline constexpr s3d::FilePathView PixelMplus12B = U"/resources/fonts/PixelMplus12-Bold.ttf";
+    };
+}
--- Re-Abyss/app/commons/Resource/Font/FontRegister.cpp
+++ Re-Abyss/app/commons/Resource/Font/FontRegister.cpp
@@ -1,4 +1,5 @@
 #include <abyss/commons/Resource/Font/FontRegister.hpp>
+#include <abyss/commons/Resource/Font/FontAssetName.hpp>
 
 namespace abyss::Resource
 {
@@ -7,13 +8,13 @@ namespace abyss::Resource
         // フォントはデバッグモードでもリソースに入れてます
 
         // タイトル
-        FontAsset::Register(U"pm12r-15", 15, U"/resources/fonts/PixelMplus12-Regular.ttf");
-        FontAsset::Register(U"pm12r-20", 20, U"/resources/fonts/PixelMplus12-Regular.ttf");
-        FontAsset::Register(U"pm12r-25", 25, U"/resources/fonts/PixelMplus12-Regular.ttf");
+        FontAsset::Register(U"pm12r-15", 15, FontAssetName::PixelMplus12);
+        FontAsset::Register(U"pm12r-20", 20, FontAssetName::PixelMplus12);
+        FontAsset::Register(U"pm12r-25", 25, FontAssetName::PixelMplus12);
 
-        FontAsset::Register(U"pm12b-12", 12, U"/resources/fonts/PixelMplus12-Bold.ttf");
-        FontAsset::Register(U"pm12b-18", 18, U"/resources/fonts/PixelMplus12-Bold.ttf");
-        FontAsset::Register(U"pm12b-20", 20, U"/resources/fonts/PixelMplus12-Bold.ttf");
+        FontAsset::Register(U"pm12b-12", 12, FontAssetName::PixelMplus12B);
+        FontAsset::Register(U"pm12b-18", 18, FontAssetName::PixelMplus12B);
+        FontAsset::Register(U"pm12b-20", 20, FontAssetName::PixelMplus12B);
 
         FontAsset::Register(U"r-12", 12, Typeface::Regular);
         FontAsset::Register(U"r-16", 16, Typeface::Regular);
