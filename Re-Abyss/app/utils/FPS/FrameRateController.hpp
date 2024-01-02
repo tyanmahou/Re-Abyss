4a7a9364d6b3bc09f1a4e07c0e045c5cbba7a307
tyanmahou <tyanmahou@gmail.com>
2024-01-02 23:40:31 +0900

    update

--- Re-Abyss/app/utils/FPS/FrameRateController.hpp
+++ Re-Abyss/app/utils/FPS/FrameRateController.hpp
@@ -1,10 +1,12 @@
 ﻿#pragma once
 #include <Siv3D/IAddon.hpp>
+#include <abyss/values/Fps.hpp>
 
 namespace abyss
 {
     class FrameRateController : IAddon
     {
-
+    private:
+        s3d::Optional<Fps> m_value;
     };
 }
