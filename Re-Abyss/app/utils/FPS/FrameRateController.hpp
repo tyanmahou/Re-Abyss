967162655c168ffb71f5f3d2849016d1f5e00409
tyanmahou <tyanmahou@gmail.com>
2024-01-05 21:24:10 +0900

    add comment

--- Re-Abyss/app/utils/FPS/FrameRateController.hpp
+++ Re-Abyss/app/utils/FPS/FrameRateController.hpp
@@ -4,6 +4,9 @@
 
 namespace abyss
 {
+    /// <summary>
+    /// フレームレート制御
+    /// </summary>
     class FrameRateController :
         public AddonSingleton<FrameRateController>,
         public s3d::IAddon
@@ -13,10 +16,21 @@ namespace abyss
         using Clock = std::chrono::steady_clock;
         using TimePoint = std::chrono::time_point<Clock>;
     public:
+        /// <summary>
+        /// フレームレートのセット
+        /// noneの場合は垂直同期
+        /// </summary>
+        /// <param name="value"></param>
         static void Set(const s3d::Optional<Fps>& value)
         {
             Instance()->set(value);
         }
+        /// <summary>
+        /// リフレッシュレート以下の場合はフレームレートのセット
+        /// そうでない場合、もしくはnoneなら垂直同期
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         static bool SetIfLessThanRefreshRate(const s3d::Optional<Fps>& value)
         {
             return Instance()->setIfLessThanRefreshRate(value);
